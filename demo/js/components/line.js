16e9992344ccb30ce15374d0895bda64c872a5b6
Ioannis Katakis <imktks@gmail.com>
2016-02-29 18:05:48 +0200

    modified line component to cooperate with the basket button component

--- demo/js/components/line.js
+++ demo/js/components/line.js
@@ -1,36 +1,51 @@
-angular.module('yds').directive('ydsLine', ['Data', function(Data){
+angular.module('yds').directive('ydsLine', ['Data', 'Filters', function(Data, Filters){
     return {
         restrict: 'E',
         scope: {
-            projectId: '@',
-            embeddable: '@',
-            embedBtnX: '@',
-            embedBtnY: '@',
-            popoverPos: '@',
-            showNavigator: '@',
-            exporting: '@',
-            elementH: '@',
-            titleSize: '@'
+            projectId: '@',     //id of the project that the data belong
+            tableType: '@',     //name of the array that contains the visualised data
+            lang: '@',          //lang of the visualised data
+            showNavigator: '@', //enable or disable line chart's navigation
+            exporting: '@',     //enable or disable the export of the chart
+            elementH: '@',      //set the height of the component
+            titleSize: '@',     //the size of the chart's main title
+
+            addToBasket: '@',   //enable or disable "add to basket" functionality, values: true, false
+            basketBtnX: '@',    //x-axis position of the basket button
+            basketBtnY: '@',    //y-axis position of the basket butto
+
+            embeddable: '@',    //enable or disabled the embedding of the component
+            embedBtnX: '@',     //x-axis position of the embed button
+            embedBtnY: '@',     //y-axis position of the embed button
+            popoverPos: '@'     //the side of the embed button from which the embed information window will appear
         },
         templateUrl: 'templates/line.html',
         link: function (scope, element, attrs) {
-            scope.enableEmbed = false;  //flag that indicates if the embed functionality is enabled
-
             var lineContainer = angular.element(element[0].querySelector('.line-container'));
 
             //create a random id for the element that will render the chart
             var elementId = "line" + Data.createRandomId();
             lineContainer[0].id = elementId;
 
-            var embeddable = scope.embeddable;
+            var projectId = scope.projectId;
+            var tableType = scope.tableType;
+            var lang = scope.lang;
             var showNavigator = scope.showNavigator;
             var exporting = scope.exporting;
             var elementH = scope.elementH;
             var titleSize = scope.titleSize;
 
-            //check if the user has enabled the embed functionality
-            if (!angular.isUndefined(embeddable) && embeddable=="true")
-                scope.enableEmbed = true;
+
+            //check if the projectId and the tableType attr is defined, else stop the process
+            if (angular.isUndefined(projectId)|| angular.isUndefined(tableType)) {
+                scope.ydsAlert = "The YDS component is not properly configured." +
+                    "Please check the corresponding documentation section";
+                return false;
+            }
+
+            //check if the language attr is defined, else assign default value
+            if(angular.isUndefined(lang))
+                lang = "en";
 
             //check if the exporting attr is defined, else assign default value
             if(angular.isUndefined(showNavigator) || (showNavigator!="true" && showNavigator!="false"))
@@ -65,7 +80,12 @@ angular.module('yds').directive('ydsLine', ['Data', function(Data){
 
                 var options = {
                     chart: {
-                        renderTo: elementId
+                        renderTo: elementId,
+                        events: {
+                            load: function(e) {
+                                Filters.addLineFilter(elementId, e.target);
+                            }
+                        }
                     },
                     rangeSelector : {
                         selected : 1
@@ -88,10 +108,17 @@ angular.module('yds').directive('ydsLine', ['Data', function(Data){
                         tooltip: {
                             valueDecimals: 2
                         }
-                    }]
+                    }],
+                    xAxis:{
+                        events: {
+                            afterSetExtremes: function (e) {
+                                Filters.addLineFilter(elementId, e.target.chart);
+                            }
+                        }
+                    }
                 };
 
-                var chart = new Highcharts.StockChart(options);
+                new Highcharts.StockChart(options);
             }, function (error) {
                 console.log('error', error);
             });
