4690db795e582b4e8a7b3a995d92c8986a7791f0
Ioannis Katakis <imktks@gmail.com>
2016-02-29 18:26:52 +0200

    modified pie component to cooperate with the basket button component

--- demo/js/components/pie.js
+++ demo/js/components/pie.js
@@ -2,35 +2,49 @@ angular.module('yds').directive('ydsPie', ['Data', function(Data){
     return {
         restrict: 'E',
         scope: {
-            projectId: '@',
-            embeddable: '@',
-            embedBtnX: '@',
-            embedBtnY: '@',
-            popoverPos: '@',
-            showLegend: '@',
-            exporting: '@',
-            elementH: '@',
-            titleSize: '@'
+            projectId: '@',     //id of the project that the data belong
+            tableType: '@',     //name of the array that contains the visualised data
+            lang: '@',          //lang of the visualised data
+            showLegend: '@',    //enable or disable the chart's legend
+            exporting: '@',     //enable or disable the export of the chart
+            elementH: '@',      //set the height of the component
+            titleSize: '@',     //the size of the chart's main title
+
+            addToBasket: '@',   //enable or disable "add to basket" functionality, values: true, false
+            basketBtnX: '@',    //x-axis position of the basket button
+            basketBtnY: '@',    //y-axis position of the basket button
+
+            embeddable: '@',    //enable or disabled the embedding of the component
+            embedBtnX: '@',     //x-axis position of the embed button
+            embedBtnY: '@',     //y-axis position of the embed button
+            popoverPos: '@'     //the side of the embed button from which the embed information window will appear
         },
         templateUrl: 'templates/pie.html',
         link: function (scope, element, attrs) {
-            scope.enableEmbed = false;  //flag that indicates if the embed functionality is enabled
-
             var pieContainer = angular.element(element[0].querySelector('.pie-container'));
 
             //create a random id for the element that will render the chart
             var elementId = "pie" + Data.createRandomId();
             pieContainer[0].id = elementId;
 
-            var embeddable = scope.embeddable;
+            var projectId = scope.projectId;
+            var tableType = scope.tableType;
+            var lang = scope.lang;
             var showLegend = scope.showLegend;
             var exporting = scope.exporting;
             var elementH = scope.elementH;
             var titleSize = scope.titleSize;
 
-            //check if the user has enabled the embed functionality
-            if (!angular.isUndefined(embeddable) && embeddable=="true")
-                scope.enableEmbed = true;
+            //check if the projectId and the tableType attr is defined, else stop the process
+            if (angular.isUndefined(projectId)|| angular.isUndefined(tableType)) {
+                scope.ydsAlert = "The YDS component is not properly configured." +
+                    "Please check the corresponding documentation section";
+                return false;
+            }
+
+            //check if the language attr is defined, else assign default value
+            if(angular.isUndefined(lang))
+                lang = "en";
 
             //check if the showLegend attr is defined, else assign default value
             if(angular.isUndefined(showLegend) || (showLegend!="true" && showLegend!="false"))
