c105a75cc4adde5f8ccaaeddbcca3e9a1e42dcca
Ioannis Katakis <imktks@gmail.com>
2016-03-18 11:17:36 +0200

    working on  yds-workbench component

--- demo/js/components/workbench.js
+++ demo/js/components/workbench.js
@@ -7,7 +7,7 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', f
 		templateUrl: 'templates/workbench.html',
 		link: function (scope, element, attrs) {
 			var userId = scope.userId;
-			var selectedVisualisation = "";
+			scope.selectedVisualisation = "";
 			scope.ydsAlert = "";
 			scope.basketList = [];
 
@@ -18,10 +18,15 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', f
 				return false;
 			}
 
-			var elementId = "workbench" + Data.createRandomId();
-			var workbenchContainer = angular.element(element[0].querySelector('.vis-area'));
-			workbenchContainer[0].id = elementId;
+			var lineChartContainer = angular.element(element[0].querySelector('.vis-area-line'));
+			var barChartContainer = angular.element(element[0].querySelector('.vis-area-bar'));
+			var pieChartContainer = angular.element(element[0].querySelector('.vis-area-pie'));
+			var mapChartContainer = angular.element(element[0].querySelector('.vis-area-map'));
 
+			lineChartContainer[0].id = "line" + Data.createRandomId();
+			barChartContainer[0].id = "bar" + Data.createRandomId();
+			pieChartContainer[0].id = "pie" + Data.createRandomId();
+			mapChartContainer[0].id = "map" + Data.createRandomId();
 
 			//custom filter for searching in basket tags or title
 			scope.customFilter = function(term){
@@ -77,75 +82,109 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', f
 				scope.slides.push(slides[i]);
 			}
 
+			/*******************************************/
+			/********* DELETE AFTER TESTING ************/
+			/******************************************/
+			scope.sampleSelectData = [
+				"ColumnA", "ColumnB", "ColumnC"
+			];
+			/*******************************************/
+			/****** END OF DELETE AFTER TESTING *******/
+			/******************************************/
 
+
+			//function that clears the visualisation area from previous charts
+			var clearVisArea = function () {
+				//check if element has client nodes and delete them.
+				if (visAreaContainer[0].childNodes.length != 0) {
+					while(visAreaContainer[0].firstChild){
+						visAreaContainer[0].removeChild(visAreaContainer[0].firstChild);
+					}
+				}
+			};
+
+
+
+			var mapInitialised = false;
 			scope.selectVisualisation = function(type) {
-
-
 				switch(type) {
 					case "line":
-						if (selectedVisualisation == "line") return;
-						//get data from server for line visualization
-						//and init an empty line visualisation
+						if (scope.selectedVisualisation != "line") {
+							//get data from server for line visualization
+							//and init an empty line visualisation
 
+							var options = {
+								chart: {
+									renderTo: lineChartContainer[0].id
+								},
+								rangeSelector : {
+									selected : 1
+								},
+								title : {
+									text : "My Line Chart"/*,
+									 style: {
+									 fontSize: titlepx"
+									 }*/
+								},
+								exporting: {
+									enabled: true
+								},
+								navigator: {
+									enabled: true
+								},
+								series : [{
+									//name : response.series,
+									//data : response.data,
+									name : "Series Name",
+									data : [],
+									tooltip: {
+										valueDecimals: 2
+									}
+								}]
+							};
 
-						var options = {
-							chart: {
-								renderTo: elementId
-							},
-							rangeSelector : {
-								selected : 1
-							},
-							title : {
-								text : "My Line Chart"/*,
-								style: {
-									fontSize: titlepx"
-								}*/
-							},
-							exporting: {
-								enabled: true
-							},
-							navigator: {
-								enabled: true
-							},
-							series : [{
-								//name : response.series,
-								//data : response.data,
-								name : "Series Name",
-								data : [],
-								tooltip: {
-									valueDecimals: 2
-								}
-							}]
-						};
-
-						new Highcharts.StockChart(options);
-
+							new Highcharts.StockChart(options);
+						}
 
 						break;
 					case "bar":
-						if (selectedVisualisation == "bar") return;
-						//get data from server for bar visualization
-						//and init an empty line visualisation
-
+						if (scope.selectedVisualisation != "bar") {
+							//get data from server for bar visualization
+							//and init an empty line visualisation
+						}
 
 						break;
 					case "pie":
-						if (selectedVisualisation == "pie") return;
-						//get data from server for pie visualization
-						//and init an empty line visualisation
-
+						if (scope.selectedVisualisation != "pie") {
+							//get data from server for pie visualization
+							//and init an empty line visualisation
+						}
 
 						break;
 					case "map":
-						if (selectedVisualisation == "map") return;
-						//get data from server for map visualization
-						//and init an empty line visualisation
+						if (scope.selectedVisualisation != "map") {
+							//get data from server for map visualization
+							//and init an empty line visualisation
 
+							if (!mapInitialised) {
+								var map = L.map(mapChartContainer[0].id, {
+									center: [37.9833333,23.7333333],
+									zoom: 5,
+									zoomControl: true
+								});
+
+								L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+									maxZoom: 18,
+									attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+								}).addTo(map);
+								mapInitialised = true;
+							}
+						}
 
 						break;
 				}
 
-				selectedVisualisation = type;
+				scope.selectedVisualisation = type;
 			}
 		}
 	};
