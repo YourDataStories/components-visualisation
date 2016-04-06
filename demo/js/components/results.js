c79ac564d474885b7fe6f863f6d5168f5b56d9d0
Ioannis Katakis <imktks@gmail.com>
2016-04-06 12:20:14 +0300

    improved pagination feature of the YDS results component

--- demo/js/components/results.js
+++ demo/js/components/results.js
@@ -1,6 +1,5 @@
-angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$templateCache', '$location', '$compile', '$ocLazyLoad', 'Search',
-    function(YDS_CONSTANTS, $window, $templateCache, $location, $compile, $ocLazyLoad, Search){
-        
+angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$templateCache', '$location', '$compile', '$ocLazyLoad', 'Search', 'Translations',
+    function(YDS_CONSTANTS, $window, $templateCache, $location, $compile, $ocLazyLoad, Search, Translations){
     return {
         restrict: 'E',
         scope: {
@@ -10,16 +9,32 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
         link: function(scope, element, attrs) {
             var resultsContainer = angular.element(element[0].querySelector('.results-content'));
             var compiledTemplates = {};
+
             scope.showNoResultsMsg = false;
             scope.results = [];
+            scope.translations = {};
+
+            //check if the language attr is defined, else assign default value
+            if(angular.isUndefined(scope.lang) || scope.lang.trim()=="")
+                scope.lang = "en";
+
+            //configure pagination options
             scope.pagination = {
                 hidden: true,
                 total: 0,
-                perPage: 2,
+                maxSize: 5,
+                perPage: 10,
                 current: 1,
+                firstText: Translations.get(scope.lang, "paginationFirstText"),
+                lastText: Translations.get(scope.lang, "paginationLastText"),
+                nextText: Translations.get(scope.lang, "paginationNextText"),
+                previousText: Translations.get(scope.lang, "paginationPreviousText"),
+
                 show: function() {
                     if (scope.pagination.hidden)
                         scope.pagination.hidden = false;
+
+                    scope.showNoResultsMsg = false;
                 },
                 hide: function() {
                     if (!scope.pagination.hidden)
@@ -27,27 +42,38 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
 
                     scope.showNoResultsMsg = true;
                 },
-                getCurrent: function() { return (scope.pagination.current - 1)*2; },
+                getCurrent: function() { return (scope.pagination.current - 1)*10; },
                 setCurrent: function(pageNum) { scope.pagination.current = pageNum; },
                 setTotal: function(resultsCount) { scope.pagination.total = resultsCount; }
             };
-            
+
             //function to prepare the templates used in search results 
             var prepareTemplates = function(){
                 //precompile the js templates for faster rendering
                 _.templateSettings.variable = "rc";
-                
+                scope.translations = Translations.getAll(scope.lang);
+
                 $ocLazyLoad.load ({
-                    files: ['templates/search/subsidy.html', 'templates/search/decision.html'],
+                    files: ['templates/search/aid-activity.html', 'templates/search/commited-item.html',
+                            'templates/search/contract.html','templates/search/decision.html',
+                            'templates/search/financial-decision.html','templates/search/non-financial-decision.html',
+                            'templates/search/organization.html', 'templates/search/public-project.html',
+                            'templates/search/subproject.html', 'templates/search/subsidy.html',
+                            'templates/search/transaction.html'],
                     cache: true
                 }).then(function() {
                     compiledTemplates = {
-                        Subsidy : _.template($templateCache.get("Subsidy.html")),
-                        Project : _.template($templateCache.get("Subsidy.html")),
-                        Decision : _.template($templateCache.get("FinancialDecision.html")),
-                        FinancialDecision : _.template($templateCache.get("FinancialDecision.html")),
-                        NonFinancialDecision : _.template($templateCache.get("FinancialDecision.html")),
-                        CommittedItem : _.template($templateCache.get("FinancialDecision.html"))
+                        AidActivity : _.template($templateCache.get("aid-activity.html")),
+                        CommittedItem : _.template($templateCache.get("commited-item.html")),
+                        Contract : _.template($templateCache.get("contract.html")),
+                        Decision : _.template($templateCache.get("decision.html")),
+                        FinancialDecision : _.template($templateCache.get("financial-decision.html")),
+                        NonFinancialDecision : _.template($templateCache.get("non-financial-decision.html")),
+                        Organization : _.template($templateCache.get("organization.html")),
+                        PublicProject : _.template($templateCache.get("public-project.html")),
+                        SubProject : _.template($templateCache.get("subproject.html")),
+                        Subsidy : _.template($templateCache.get("subsidy.html")),
+                        Transaction : _.template($templateCache.get("transaction.html"))
                     };
                 });
             };
@@ -58,17 +84,17 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
                 scope.pagination.setCurrent(pageNumber);
 
                // Search.performSearch(searchTerm, pageLimit, scope.pagination.getCurrent())
-                Search.performSearch(searchTerm, 2, scope.pagination.getCurrent())
+                Search.performSearch(searchTerm, 10, scope.pagination.getCurrent())
                 .then(function (response) {
                     var resultsCount = response.data.response.numFound;
                     scope.results = angular.copy(response.data.response.docs);
 
                     //clear the existing results
                     resultsContainer[0].innerHTML = "";
-
+                    scope.pagination.setTotal(resultsCount);
+                    
                     //check if the length of the results is greater than 0, and handle pagination
                     if (resultsCount>0) {
-                        scope.pagination.setTotal(resultsCount);
                         scope.pagination.show();
                     } else {
                         scope.pagination.hide();
@@ -76,16 +102,28 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
                     }
 
                     //iterate through the results and get the template based on the type of the result
-                    _.each(scope.results, function(result, i) {
+                    _.each(scope.results, function(result, index) {
+                        result._hidden = true;
+
                         var templateData = {
-                            attrHeaders: translations,
+                            attrHeaders: scope.translations,
                             lang: scope.lang,
-                            result: scope.results[i]
+                            result: scope.results[index],
+                            index: index
                         };
 
                         //fill the data of the template and append it to the result list
-                        var content = compiledTemplates[_.first(result.type)](templateData);
-                        resultsContainer.append(content);
+                        var resultTemplate = "";
+                        for(var j=0; j<result.type.length; j++) {
+                            resultTemplate = compiledTemplates[result.type[j]];
+                            if(!_.isUndefined(resultTemplate))
+                                break;
+                        }
+
+                        if (!_.isUndefined(resultTemplate)) {
+                            var content = $compile(resultTemplate(templateData))(scope);
+                            resultsContainer.append(content);
+                        }
                     });
                 }, function (error) {
                     scope.pagination.hide();
@@ -126,7 +164,7 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
             scope.$on("$destroy", function() {
                 Search.clearKeyword();
             });
-            
+
             prepareTemplates();
         },
         controller: function($scope) {
@@ -138,12 +176,16 @@ angular.module('yds').directive('ydsResults', ['YDS_CONSTANTS', '$window', '$tem
                     vizType : vizType
                 });
             };
+            
+            $scope.showMore = function (projectIndex) {
+                var selectedItem = $scope.results[projectIndex];
+                selectedItem._hidden = !selectedItem._hidden;
+            };
 
-            $scope.visitResult = function(project) {
-                debugger;
-                var redirectURL = YDS_CONSTANTS.PROJECT_DETAILS_URL + '?id=' + project.attributes['project-id'];
+            $scope.visitResult = function(projectId) {
+                var redirectURL = YDS_CONSTANTS.PROJECT_DETAILS_URL + '?id=' + projectId;
                 $window.location.href = redirectURL;
-            }
+            };
         }
     };
 }]);
