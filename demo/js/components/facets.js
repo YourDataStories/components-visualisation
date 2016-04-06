801a81e51c15f3066127eebb5160a56bf8f333fc
Ioannis Katakis <imktks@gmail.com>
2016-04-06 20:32:22 +0300

    enhancements on the functionality of the YDS facet component

--- demo/js/components/facets.js
+++ demo/js/components/facets.js
@@ -1,4 +1,5 @@
-angular.module('yds').directive('ydsFacets', ['Data', 'Search', function(Data, Search){
+angular.module('yds').directive('ydsFacets', ['Data', 'Search', '$location', '$window', 'YDS_CONSTANTS',
+	function(Data, Search, $location, $window, YDS_CONSTANTS){
 	return {
 		restrict: 'E',
 		scope: {},
@@ -10,18 +11,27 @@ angular.module('yds').directive('ydsFacets', ['Data', 'Search', function(Data, S
 			scope.checkboxFacets = [];
 			scope.rangeFacets = [];
 
+			var initFacetArrays = function() {
+				scope.rawAppliedFacets = [];
+				scope.checkboxFacets = [];
+				scope.rangeFacets = [];
+			};
+
 			var formatCheckboxFacets = function (checkboxFacets, facetsView) {
-				_.each(_.keys(checkboxFacets), function(facetName){
-					var rawFacetOptions = checkboxFacets[facetName];
+				_.each(_.keys(checkboxFacets), function(facetAttr){
+					var rawFacetOptions = checkboxFacets[facetAttr];
+
 					var newFacet = {
-						facet_name: _.findWhere(facetsView, {attribute: facetName}).header,
+						facet_name: _.findWhere(facetsView, {attribute: facetAttr}).header,
+						facet_attribute: facetAttr,
 						facet_options: []
 					};
 
 					for(var i=0; i<rawFacetOptions.length; i+=2) {
 						newFacet.facet_options.push({
 							name: rawFacetOptions[i],
-							value: rawFacetOptions[i+1]
+							value: rawFacetOptions[i+1],
+							selected: false
 						});
 					}
 
@@ -30,18 +40,23 @@ angular.module('yds').directive('ydsFacets', ['Data', 'Search', function(Data, S
 			};
 
 			var formatRangeFacets = function (rangeFacets, facetsView) {
-				_.each(_.keys(rangeFacets), function(facetName){
-					var rawFacetOptions = rangeFacets[facetName];
-					
+				_.each(_.keys(rangeFacets), function(facetAttr){
+					var rawFacetOptions = rangeFacets[facetAttr];
+
 					var newFacet = {
-						facet_name: _.findWhere(facetsView, {attribute: facetName}).header,
+						facet_name: _.findWhere(facetsView, {attribute: facetAttr}).header,
+						facet_attribute: facetAttr,
 						facet_options: {
 							model: rawFacetOptions.start,
 							high: rawFacetOptions.end,
 							options: {
+								id: facetAttr,
 								hideLimitLabels: true,
 								floor: rawFacetOptions.start,
 								ceil: rawFacetOptions.end,
+								onChange: function () {
+									scope.applyFacets();
+								}
 								/*translate: function (value, sliderId, label) {
 									switch (label) {
 										case 'model':
@@ -57,11 +72,11 @@ angular.module('yds').directive('ydsFacets', ['Data', 'Search', function(Data, S
 					};
 
 					scope.rangeFacets.push(newFacet);
-					debugger;
 				});
 			};
 
 			var updateFacets = function() {
+				initFacetArrays();
 				var newFacets = Search.getFacets();
 				var facetsView = Search.getFacetsView();
 
@@ -69,9 +84,33 @@ angular.module('yds').directive('ydsFacets', ['Data', 'Search', function(Data, S
 				formatRangeFacets(newFacets.facet_ranges, facetsView);
 			};
 
+
+			//function to transform the facet list in format that is valid
+			//to pass it as a url param
 			scope.applyFacets = function () {
+				var facetUrlString = "";
+
+				var checkboxFacets = scope.checkboxFacets;
+				_.each(checkboxFacets, function (facet) {
+					var activeFacets = _.pluck( _.where(facet.facet_options, {selected: true}), 'name').join();
+					if (activeFacets.trim().length>0)
+						facetUrlString+= "&fq=" + facet.facet_attribute + ":" + activeFacets;
+				});
+
+				var rangeFacets = scope.rangeFacets;
+				_.each(rangeFacets, function (facet) {
+					facetUrlString+= "&fq=" + facet.facet_options.options.id
+											+ "["
+											+ facet.facet_options.model
+											+ "+TO+"
+											+ facet.facet_options.high
+											+ "]";
+				});
 				
-			}
+				var urlParams = "?q="+ $location.search().q + facetUrlString;
+				$window.location.href = (YDS_CONSTANTS.SEARCH_RESULTS_URL + urlParams);
+			};
+
 			Search.registerFacetsCallback(updateFacets)
 		}
 	};
