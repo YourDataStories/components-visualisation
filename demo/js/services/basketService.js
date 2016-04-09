f664450f484f9d6c97aa17cfd350350af0eafb3f
Ioannis Katakis <imktks@gmail.com>
2016-04-09 13:47:37 +0300

    moved basket modal functions inside the Basket service, added dedicated controller for the basket modal

--- demo/js/services/basketService.js
+++ demo/js/services/basketService.js
@@ -1,6 +1,8 @@
-angular.module('yds').factory('Basket', [ 'YDS_CONSTANTS', '$q', '$http', function (YDS_CONSTANTS, $q, $http) {
+angular.module('yds').factory('Basket', [ 'YDS_CONSTANTS', '$q', '$http', '$uibModal',
+function (YDS_CONSTANTS, $q, $http, $uibModal) {
 	var basketCallbacks = [];
 	var lastSavedItem = {};
+	var modalInstance = {};
 
 	var notifyObservers = function (observerStack) { //function to trigger the callbacks of observers
 		angular.forEach(observerStack, function (callback) {
@@ -8,40 +10,53 @@ angular.module('yds').factory('Basket', [ 'YDS_CONSTANTS', '$q', '$http', functi
 		});
 	};
 
+	//private function accessible only through the BasketModalCtrl in order to close the modalInstance
+	var closeModal = function () {
+		modalInstance.close();
+	};
+
 	return {
 		registerCallback: function(callback) { basketCallbacks.push(callback); },
 		getLastSavedItem: function() { return lastSavedItem; },
-		createItem: function(userId) {
-			return {
-				user_id: userId,
-				component_parent_uuid: "",
-				title: "",
-				tags: [],
-				filters: [],
-				component_type: "",
-				content_type: "",
-				type: "",
-				is_private: true,
-				lang: ""
+		openModal: function(basketInput) {
+			var modalInput = {
+				title: "Add to Basket",
+				closeModal: closeModal
 			};
-		},
-		initializeItem: function (bskItem) {
-			bskItem.title = "";
-			bskItem.type = "";
-			bskItem.is_private = true;
-			bskItem.tags = [];
-			bskItem.filters = [];
 
-			return bskItem;
+			modalInstance = $uibModal.open({
+				animation: false,
+				controller: 'BasketModalCtrl',
+				templateUrl: 'templates/basket-modal.html',
+				size: 'md',
+				resolve: {
+					basketInput: function () {
+						return basketInput;
+					},
+					modalInput: function() {
+						return modalInput;
+					}
+				}
+			});
+
+			return modalInstance;
 		},
-		initializeModalItem: function () {
-			return {
-				alert: "",
-				title: "",
-				tags: "",
-				type: "Dataset",
-				is_private: true
-			};
+		formatBasketTags : function (tagsString) {
+			//tokenize the basket item tags and save them to the basket item obj
+			//if not tag is available, assign the default value "untagged"
+			var basketTags = [];
+			tagsString = tagsString.trim();
+			if (tagsString.length>0) {
+				var tmpTags = tagsString.split(',');
+
+				_.each(tmpTags, function(tag){
+					basketTags.push(tag.trim());
+				});
+			} else {
+				basketTags.push("untagged");
+			}
+
+			return basketTags;
 		},
 		saveBasketItem: function(bskItem) {
 			var deferred = $q.defer();
@@ -52,11 +67,10 @@ angular.module('yds').factory('Basket', [ 'YDS_CONSTANTS', '$q', '$http', functi
 				url: YDS_CONSTANTS.BASKET_URL + "save",
 				headers: {'Content-Type': 'application/json'},
 				data: JSON.stringify(bskItem)
-			})
-				.success(function (data) {
-					notifyObservers(basketCallbacks);
-					deferred.resolve(data);
-				}).error(function (error) {
+			}).success(function (data) {
+				notifyObservers(basketCallbacks);
+				deferred.resolve(data);
+			}).error(function (error) {
 				deferred.reject(error);
 			});
 
@@ -109,3 +123,59 @@ angular.module('yds').factory('Basket', [ 'YDS_CONSTANTS', '$q', '$http', functi
 		}
 	}
 }]);
+
+
+angular.module('yds').controller('BasketModalCtrl', function ($scope, modalInput, basketInput, Basket) {
+	//configuration of the shown modal
+	$scope.modalConfig = {
+		closeModal: modalInput.closeModal,
+		title: modalInput.title,
+		alert: ""
+	};
+
+	//prepare the basket item
+	$scope.basketObj = {
+		title: "",
+		type: "Dataset",
+		filters: basketInput.filters,
+		lang: basketInput.lang,
+		user_id: basketInput.user_id,
+		content_type: basketInput.content_type,
+		component_parent_uuid: basketInput.component_parent_uuid,
+		component_type: basketInput.component_type,
+		is_private: true,
+		tags: ""
+	};
+
+	//function to save a basket item
+	$scope.saveBasketItem = function () {
+		if ($scope.basketObj.title.trim().length==0) {
+			$scope.modalConfig.alert = "Please provide a title for your item";
+			return false;
+		} else
+			$scope.basketObj.tags = Basket.formatBasketTags($scope.basketObj.tags);
+
+	//call the service to check if basket item exists
+
+		Basket.saveBasketItem($scope.basketObj)
+		.then(function(response){
+			$scope.clearModalWarnings($scope.modalConfig);
+			$scope.dismissModal();
+		}, function(error){
+			$scope.modalConfig.alert = "An error occurred, please try again";
+		});
+	};
+	
+	//function to clear modal warning messages
+	$scope.clearModalWarnings = function (modalConfig) {
+		if (modalConfig.alert.trim().length>0)
+			modalConfig.alert = "";
+	};
+
+	//function to be called when the modal's cancel button is pressed
+	$scope.dismissModal = function() {
+		$scope.modalConfig.closeModal();
+	}
+});
+
+
