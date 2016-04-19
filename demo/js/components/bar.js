a4563fe55c95901702c2e97fcbed7a3eac9904f2
Ioannis Katakis <imktks@gmail.com>
2016-04-19 13:56:09 +0300

    minor fix in bar chart components

--- demo/js/components/bar.js
+++ demo/js/components/bar.js
@@ -135,7 +135,7 @@ angular.module('yds').directive('ydsBar', ['Data', function(Data){
                     tooltip: {
                         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                        '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+                        '<td style="padding:0"><b>{point.y:.0f}</b></td></tr>',
                         footerFormat: '</table>',
                         shared: true,
                         useHTML: true
