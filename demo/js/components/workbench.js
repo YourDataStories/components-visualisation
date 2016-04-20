073b38a09e11f89a357c3dd61edd0ee813b05374
Ioannis Katakis <imktks@gmail.com>
2016-04-20 20:17:18 +0300

    added bar chart visualization in  workbench

--- demo/js/components/workbench.js
+++ demo/js/components/workbench.js
@@ -1,5 +1,5 @@
-angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '$window', 'Workbench',
-	function(Data, Basket, $timeout, $window, Workbench){
+angular.module('yds').directive('ydsWorkbench', ['$ocLazyLoad', '$timeout', '$window', 'Data', 'Basket', 'Workbench',
+	function($ocLazyLoad, $timeout, $window, Data, Basket, Workbench){
 	return {
 		restrict: 'E',
 		scope: {
@@ -22,50 +22,24 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '
 				scope.lang = "en";
 
 			var lineChartContainer = angular.element(element[0].querySelector('.vis-area-line'));
+			var barChartContainer = angular.element(element[0].querySelector('.vis-area-bar'));
 			var pieChartContainer = angular.element(element[0].querySelector('.vis-area-pie'));
-			var mapChartContainer = angular.element(element[0].querySelector('.vis-area-map'));
 
 			lineChartContainer[0].id = "line" + Data.createRandomId();
+			barChartContainer[0].id = "bar" + Data.createRandomId();
 			pieChartContainer[0].id = "pie" + Data.createRandomId();
-			mapChartContainer[0].id = "map" + Data.createRandomId();
-			
+
+
+			scope.workbench = Workbench.init();
+			scope.slidesConfig = Workbench.getSlidesConfig();
+			scope.lineChart = Workbench.initLineChart(lineChartContainer[0].id);
+			scope.barChart = Workbench.initBarChart(barChartContainer[0].id);
+
 			scope.basketVars = {
 				selBasketIds: [],
 				basketList: [],
 				searchText: ""
 			};
-			
-			scope.lineChart = {
-				data: [],
-				chart: {},
-				options: {
-					chart: { renderTo: lineChartContainer[0].id },
-					rangeSelector : { enabled: false },
-					scrollbar : { enabled: false },
-					title : { text : "Not available" },
-					exporting: { enabled: true },
-					navigator: { enabled: false },
-					series : []
-				},
-				selectedAxisX: "",
-				selectedAxisYa: "",
-				selectedAxisYb: "",
-				selectedAxisYc: "",
-				axisX: [],
-				axisY: []
-			};
-			
-			scope.workbenchVars = {
-				selectedVis: "default",
-				availableViews: [],
-				availableViewsRaw: [],
-				selectedView: "",
-				selectedViewObj: {}
-			};
-
-			scope.noWrapSlides = false;
-			scope.active = 0;
-			scope.slides = Workbench.getSlides();
 
 			//get all the basket items of the user
 			var getBasketItems = function () {
@@ -102,21 +76,21 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '
 
 				Workbench.getAvailableVisualisations(scope.lang, scope.basketVars.selBasketIds)
 					.then(function(response){
-						scope.workbenchVars.availableViews = _.pluck(response.data, 'type');
-						scope.workbenchVars.availableViewsRaw = response.data;
+						scope.workbench.availableViews = _.pluck(response.data, 'type');
+						scope.workbench.availableViewsRaw = response.data;
 					}, function(error) {
-						debugger
+						console.log("error in getAvailableVisualisations function")
 					});
 			};
 
 			//function called when user selects one of the available views
 			scope.selectView = function(viewName) {
-				scope.workbenchVars.selectedViewObj = _.findWhere(scope.workbenchVars.availableViewsRaw, {type: viewName});
+				scope.workbench.selectedViewObj = _.findWhere(scope.workbench.availableViewsRaw, {type: viewName});
 
-				if (!_.isUndefined(scope.workbenchVars.selectedViewObj)) {
-					var viewComponents = _.pluck(scope.workbenchVars.selectedViewObj.values, 'component');
+				if (!_.isUndefined(scope.workbench.selectedViewObj)) {
+					var viewComponents = _.pluck(scope.workbench.selectedViewObj.values, 'component');
 
-					_.each(scope.slides, function(slideView){
+					_.each(scope.slidesConfig.slides, function(slideView){
 						_.each(slideView.images, function(slide){
 							if(_.contains(viewComponents, slide.type) && slide.visible==false ) {
 								slide.visible = true;
@@ -124,8 +98,8 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '
 						});
 					})
 				} else {
-					scope.workbenchVars.selectedVis = "default"
-					_.each(scope.slides, function(slideView){
+					scope.workbench.selectedVis = "default";
+					_.each(scope.slidesConfig.slides, function(slideView){
 						_.each(slideView.images, function(slide){
 							slide.visible = false;
 						});
@@ -134,7 +108,241 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '
 				}
 			};
 
-			getBasketItems();
+
+			/**********************************************/
+			/************ LINE CHART FUNCTIONS ************/
+			/**********************************************/
+
+			/**
+			 * filter function applied on the in comboboxes which are used
+			 * for the configuration of the line chart visualization
+			 **/
+			scope.lineChartFilter= function (index) {
+				return function (item) {
+					var attrSelected = false;
+					var axisYConfig = angular.copy(scope.lineChart.axisYConfig);
+
+					//if the filtered attribute is already selected, return it
+					if (axisYConfig[index].selected!=null && axisYConfig[index].selected.attribute == item.attribute)
+						return item;
+
+					//else search if the attribute is selected in one of the other compoboxes
+					axisYConfig.splice(index, 1);
+					if (axisYConfig.length > 0) {
+						var existingCombos = _.where(_.pluck(axisYConfig, 'selected'), {attribute: item.attribute});
+
+						if (existingCombos.length > 0)
+							attrSelected = true;
+					}
+
+					//if the attribute is not selected in none of the comboboxes return it as available
+					if (!attrSelected)
+						return item;
+				};
+
+			};
+
+			/**
+			 * functions used to add or remove comboboxes to/from the line chart configuration
+			 **/
+			scope.addLineAxisY = Workbench.addLineAxisY;
+			scope.removeLineAxisY = Workbench.removeLineAxisY;
+
+
+			/**
+			 * function used to create the line chart visualization based on the supplied configuration
+			 **/
+			var createLineChart = function(lineInput) {
+				//remove the existing line chart series
+				if(!_.isUndefined(scope.lineChart.chart.series)) {
+
+					while(scope.lineChart.chart.series.length > 0)
+						scope.lineChart.chart.series[0].remove(true);
+				}
+
+				//get the data and the title of the chart
+				scope.lineChart.data = lineInput.data.data;
+				scope.lineChart.title = Data.deepObjSearch(lineInput.data, lineInput.view[0].attribute + "." + scope.lang)
+
+				//if the line chart is visualized for the first time, create it
+				if (!Workbench.getLineChartStatus()) {
+					scope.lineChart.chart = new Highcharts.StockChart(scope.lineChart.options);
+					scope.lineChart.initialized = true;
+				}
+
+				//set the title of the line chart and add its series
+				scope.lineChart.chart.setTitle({ text: scope.lineChart.title });
+				scope.lineChart.chart.addSeries({
+					name : scope.lineChart.series,
+					data : scope.lineChart.data,
+					tooltip: {
+						valueDecimals: 2
+					}
+				});
+
+				scope.lineChart.chart.redraw();
+			};
+
+
+			/**********************************************/
+			/************ BAR CHART FUNCTIONS *************/
+			/**********************************************/
+
+			/**
+			 * filter function applied on the in comboboxes which are used
+			 * for the configuration of the line chart visualization
+			 **/
+			scope.barChartFilter= function (index) {
+				return function (item) {
+					var attrSelected = false;
+					var axisYConfig = angular.copy(scope.barChart.axisYConfig);
+
+					//if the filtered attribute is already selected, return it
+					if (axisYConfig[index].selected!=null && axisYConfig[index].selected.attribute == item.attribute)
+						return item;
+
+					//else search if the attribute is selected in one of the other compoboxes
+					axisYConfig.splice(index, 1);
+					if (axisYConfig.length > 0) {
+						var existingCombos = _.where(_.pluck(axisYConfig, 'selected'), {attribute: item.attribute});
+
+						if (existingCombos.length > 0)
+							attrSelected = true;
+					}
+
+					//if the attribute is not selected in none of the comboboxes return it as available
+					if (!attrSelected)
+						return item;
+				};
+
+			};
+
+
+			/**
+			 * functions used to add or remove comboboxes to/from the bar chart configuration
+			 **/
+			scope.addBarAxisY = Workbench.addBarAxisY;
+			scope.removeBarAxisY = Workbench.removeBarAxisY;
+
+			
+			/**
+			 * function used to create the bar chart visualization based on the supplied configuration
+			 **/
+			var createBarChart = function(barInput) {
+				//remove the existing bar chart series
+				if(!_.isUndefined(scope.barChart.chart.series)) {
+					while(scope.barChart.chart.series.length > 0)
+						scope.barChart.chart.series[0].remove(true);
+				}
+
+				//get the data, the categories and the title of the chart
+				scope.barChart.data = barInput.data.data;
+				scope.barChart.categories = barInput.data.categories;
+				scope.barChart.title = Data.deepObjSearch(barInput.data, barInput.view[0].attribute);
+
+				//if the line chart is visualized for the first time, create it
+				if (!Workbench.getBarChartStatus()) {
+					scope.barChart.chart = new Highcharts.Chart(scope.barChart.options);
+					scope.barChart.initialized = true;
+				}
+
+				//set the title of the line chart and add its series
+				scope.barChart.chart.setTitle({ text: scope.lineChart.title });
+				scope.barChart.chart.xAxis[0].setCategories(scope.barChart.categories);
+				_.each(scope.barChart.data, function(serie){
+					scope.barChart.chart.addSeries(serie);
+				});
+
+				scope.barChart.chart.redraw();
+			};
+
+
+			/**********************************************/
+			/***** GENERAL - VISUALIZATION FUNCTIONS ******/
+			/**********************************************/
+			scope.selectVisualisation = function(slideId, type) {
+				if (!Workbench.checkVisAvailability(slideId, type)) {
+					return false;
+				} else {
+					scope.workbench.selectedVis = type;
+
+					switch(scope.workbench.selectedVis) {
+						case "linechart":
+							var linechartInput = _.findWhere(scope.workbench.selectedViewObj.values, {component: type});
+
+							if (!_.isUndefined(linechartInput)) {
+								scope.lineChart.axisX = linechartInput["axis-x"];
+								scope.lineChart.axisY = linechartInput["axis-y"];
+
+								if(scope.lineChart.axisX.length==1)
+									scope.lineChart.selectedAxisX = scope.lineChart.axisX[0];
+
+								scope.lineChart.axisYConfig = [{
+									selected: "",
+									options: scope.lineChart.axisY
+								}]
+							}
+
+							break;
+						case "barchart":
+							var barchartInput = _.findWhere(scope.workbench.selectedViewObj.values, {component: type});
+
+							if (!_.isUndefined(barchartInput)) {
+								scope.barChart.axisX = barchartInput["axis-x"];
+								scope.barChart.axisY = barchartInput["axis-y"];
+
+								if(scope.barChart.axisX.length==1)
+									scope.barChart.selectedAxisX = scope.barChart.axisX[0];
+
+								scope.barChart.axisYConfig = [{
+									selected: "",
+									options: scope.barChart.axisY
+								}];
+							}
+							
+							break;
+					}
+				}
+			};
+
+
+			/**
+			 * function called when "Visualise Data" is clicked, which is responsible to fetch the data of the
+			 * selected visualization and call the corresponding chart visualization function
+			 **/
+			scope.createVisualization = function() {
+				switch(scope.workbench.selectedVis) {
+					case "linechart":
+						Workbench.getLineBarVis(scope.workbench.selectedVis, scope.workbench.selectedView, scope.lineChart.axisX[0].attribute,
+							scope.lineChart.axisY[0].attribute, scope.basketVars.selBasketIds)
+							.then(function (response) {
+								createLineChart(response);
+							}, function (error) {
+								console.log("error in linechart - getLineBarVis function")
+							});
+
+						break;
+					case "barchart":
+						var axisYAttrs = [];
+						_.forEach(scope.barChart.axisYConfig, function(axis){
+							if (axis.selected!=null && !_.isUndefined(axis.selected) && _.isObject(axis.selected))
+								axisYAttrs.push(axis.selected.attribute);
+						});
+
+						axisYAttrs = axisYAttrs.join(",");
+
+						if (axisYAttrs.length > 0) {
+							Workbench.getLineBarVis(scope.workbench.selectedVis, scope.workbench.selectedView, scope.barChart.axisX[0].attribute, axisYAttrs, scope.basketVars.selBasketIds)
+							.then(function (response) {
+								createBarChart(response);
+							}, function (error) {
+								console.log("error in barchart - getLineBarVis function")
+							});
+						}
+
+						break;
+				}
+			};
 
 
 			var triggerResizeEvt = function() {
@@ -142,109 +350,21 @@ angular.module('yds').directive('ydsWorkbench', ['Data', 'Basket', '$timeout', '
 				evt.initUIEvent('resize', true, false, $window, 0);
 				$window.dispatchEvent(evt);
 			};
-			
-			scope.createVisualization = function() {
-				Workbench.generateLinechart(scope.workbenchVars.selectedView, scope.lineChart.axisX[0].attribute,
-											scope.lineChart.axisY[0].attribute, scope.basketVars.selBasketIds)
-					.then(function(response){
-							scope.lineChart.data = response.data.data;
-							scope.lineChart.title = Data.deepObjSearch(response.data, response.view[0].attribute + "." + scope.lang)
 
-                            scope.lineChart.chart.setTitle({ text: scope.lineChart.title });
-							scope.lineChart.chart.addSeries({
-								name : scope.lineChart.series,
-								data : scope.lineChart.data,
-								tooltip: {
-									valueDecimals: 2
-								}
-							});
 
-							scope.lineChart.chart.redraw();
-					}, function (error) {
-						debugger;
-					})
+			//function to prepare the templates used in search results 
+			var loadTemplates = function(){
+				$ocLazyLoad.load ({
+					files: ['templates/workbench/linechart-config.html',
+							'templates/workbench/barchart-config.html'],
+					cache: true
+				}).then(function(response) {
+					console.log("Templates properly loaded:", response);
+				});
 			};
-			
-			var lineInit = barInit = pieInit = mapInit = false;
-			scope.selectVisualisation = function(slideId, type) {
-				if (!Workbench.checkVisAvailability(slideId, type)) {
-					return false;
-				} else {
-					scope.workbenchVars.selectedVis = type;
 
-					switch(scope.workbenchVars.selectedVis) {
-						case "linechart":
-							var linechartInput = _.findWhere(scope.workbenchVars.selectedViewObj.values, {component: type})//;.values;
-
-							if (!_.isUndefined(linechartInput)) {
-								scope.lineChart.axisX = linechartInput["axis-x"];
-								scope.lineChart.axisY = linechartInput["axis-y"];
-							}
-
-							break;
-					}
-				}
-
-				switch(type) {
-					case "linechart":
-						if (!lineInit) {
-							scope.lineChart.chart = new Highcharts.StockChart(scope.lineChart.options);
-							lineInit = true;
-
-							$timeout(function(){
-								triggerResizeEvt();
-							});
-						}
-
-						break;
-					/*case "pie":
-						if (scope.selectedVisualisation != "pie") {
-							//get data from server for pie visualization
-							//and init an empty line visualisation
-						}
-
-						if (!pieInit) {
-							var options = {
-								chart: {
-									plotBackgroundColor: null,
-									plotBorderWidth: null,
-									plotShadow: false,
-									type: 'pie',
-									renderTo: pieChartContainer[0].id
-								},
-								title: { text: "My Pie Chart" },
-								tooltip: {
-									pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-								},
-								legend: { enabled: true },
-								exporting: { enabled: true },
-								plotOptions: {
-									pie: {
-										allowPointSelect: true,
-										cursor: 'pointer',
-										dataLabels: {
-											enabled: false
-										},
-										showInLegend: true
-									}
-								},
-								series: [{
-									name: [],
-									colorByPoint: true,
-									data: []
-								}]
-							};
-
-							$timeout(function(){
-								triggerResizeEvt();
-								new Highcharts.Chart(options);
-								pieInit = true;
-							});
-						}
-
-						break;*/
-				}
-			}
+			loadTemplates();
+			getBasketItems();
 		}
 	};
 }]);
--- demo/js/services/workbenchService.js
+++ demo/js/services/workbenchService.js
@@ -1,6 +1,10 @@
 angular.module('yds').factory('Workbench', [ 'YDS_CONSTANTS', '$q', '$http', 'Data',
 	function (YDS_CONSTANTS, $q, $http, Data) {
-		var slides = [{
+
+	var slidesConfig = {
+		noWrap: false,
+		active: 0,
+		slides: [{
 			id:0,
 			images : [ {
 				src: "img/thumbnails/line_chart.png",
@@ -9,55 +13,201 @@ angular.module('yds').factory('Workbench', [ 'YDS_CONSTANTS', '$q', '$http', 'Da
 				visible: false
 			}, {
 				src: "img/thumbnails/bar_chart.png",
-				name: "Grid",
-				type: "grid",
+				name: "Bar Chart",
+				type: "barchart",
 				visible: false
 			}, {
 				src: "img/thumbnails/pie_chart.png",
 				name: "Pie Chart",
-				type: "pie",
+				type: "piechart",
 				visible: false
 			}]
-		}
-		];
+		}]
+	};
 
-		return {
-			generateLinechart: function(viewType, xAxis, yAxis, basketIds) {
-				var deferred = $q.defer();
+	var workbenchConfig = {};
+	var lineChartConfig = {};
+	var barChartConfig = {};
 
-				//call the service with POST method
-				$http({
-					method: 'POST',
-					url: "http://"+ YDS_CONSTANTS.API_INTERACTIVE_LINE + "?type=" + viewType + "&axis-x=" + xAxis + "&axis-y=" + yAxis,
-					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-					data: Data.transform(JSON.stringify(basketIds))
-				}).success(function (data) {
-					deferred.resolve(data);
-				}).error(function (error) {
-					deferred.reject(error);
-				});
+	return {
+		init: function() {
+			//iterate through all the available vis options of the carousel and deactivate them
+			_.each(slidesConfig.slides, function(slideView){
+				_.each(slideView.images, function(image) {
+					image.visible = false;
+				})
+			});
 
-				return deferred.promise;
-			}, getAvailableVisualisations: function(lang, basketIds) {
-				var deferred = $q.defer();
+			//initialize the configuration options of workbench
+			workbenchConfig = {
+				selectedVis: "default",
+				availableViews: [],
+				availableViewsRaw: [],
+				selectedView: "",
+				selectedViewObj: {}
+			};
 
-				//call the service with POST method
-				$http({
-					method: 'POST',
-					url: "http://"+ YDS_CONSTANTS.API_PLOT_INFO + "?lang=" + lang ,
-					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-					data: Data.transform(JSON.stringify(basketIds))
-				}).success(function (data) {
-					deferred.resolve(data);
-				}).error(function (error) {
-					deferred.reject(error);
-				});
+			return workbenchConfig;
+		},
+		initLineChart: function (elementId) {
+			//initialize the configuration options of the line chart component
+			lineChartConfig = {
+				initialized: false,
+				data: [],
+				chart: {},
+				options: {
+					chart: { renderTo: elementId },
+					rangeSelector : { enabled: false },
+					scrollbar : { enabled: false },
+					title : { text : "Not available" },
+					exporting: { enabled: true },
+					navigator: { enabled: false },
+					series : []
+				},
+				selectedAxisX: "",
+				axisX: [],
+				axisY: [],
+				axisYConfig: []
+			};
 
-				return deferred.promise;
-			},
-			getSlides : function() { return slides; },
-			checkVisAvailability: function (slideId, visType) {
-				return _.findWhere(slides[slideId].images, {type: visType}).visible;
+			return lineChartConfig;
+		},
+		initBarChart: function(elementId) {
+			//initialize the configuration options of the bar chart component
+			barChartConfig = {
+				initialized: false,
+				data: [],
+				categories: [],
+				chart: {},
+				selectedAxisX: "",
+				axisX: [],
+				axisY: [],
+				axisYConfig: [],
+				options: {
+					chart: {
+						type: 'column',
+						renderTo: elementId
+					},
+					title: { text: "Not available" },
+					xAxis: { categories: [] },
+					legend: { enabled: true },
+					exporting: { enabled: true },
+					tooltip: {
+						headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+						pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+						'<td style="padding:0"><b>{point.y:.0f}</b></td></tr>',
+						footerFormat: '</table>',
+						shared: true,
+						useHTML: true
+					},
+					plotOptions: {
+						column: {
+							pointPadding: 0.2,
+							borderWidth: 0
+						}
+					},
+					series: []
+				}
+			};
+
+			return barChartConfig;
+		},
+		getSlidesConfig : function() { return slidesConfig; },
+		checkVisAvailability: function (slideId, visType) { return _.findWhere(slidesConfig.slides[slideId].images, {type: visType}).visible; },
+		getLineBarVis: function(visType, viewType, xAxis, yAxis, basketIds) {
+			var visUrl = "";
+			var deferred = $q.defer();
+
+			switch(visType) {
+				case "linechart":
+					visUrl = "http://" + YDS_CONSTANTS.API_INTERACTIVE_LINE;
+					break;
+				case "barchart":
+					visUrl = "http://" + YDS_CONSTANTS.API_INTERACTIVE_BAR;
+					break;
 			}
+
+			$http({
+				method: 'POST',
+				url: visUrl + "?type=" + viewType + "&axis-x=" + xAxis + "&axis-y=" + yAxis,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				data: Data.transform(JSON.stringify(basketIds))
+			}).success(function (data) {
+				deferred.resolve(data);
+			}).error(function (error) {
+				deferred.reject(error);
+			});
+
+			return deferred.promise;
+		},
+		getAvailableVisualisations: function(lang, basketIds) {
+			var deferred = $q.defer();
+
+			//call the service with POST method
+			$http({
+				method: 'POST',
+				url: "http://"+ YDS_CONSTANTS.API_PLOT_INFO + "?lang=" + lang ,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+				data: Data.transform(JSON.stringify(basketIds))
+			}).success(function (data) {
+				deferred.resolve(data);
+			}).error(function (error) {
+				deferred.reject(error);
+			});
+
+			return deferred.promise;
+		},
+		getLineChartStatus: function() { return lineChartConfig.initialized;  },
+		addLineAxisY: function() {			// function used to add combobox entries for the configuration of the line chart visualization
+			// check if there is any combobox with default value
+			var nonSelectedCombo = _.where(lineChartConfig.axisYConfig, {selected: ""});
+
+			// if there is an empty combobox or the number of comboboxes is equals with number
+			// of the line axis, stop the excecution of the function
+			if ( nonSelectedCombo.length>0 ||
+				lineChartConfig.axisYConfig.length>2 ||
+				lineChartConfig.axisYConfig.length == lineChartConfig.axisY.length ) {
+
+				return false;
+			} else {
+				var newCombo = {
+					selected: "",
+					options: lineChartConfig.axisY
+				};
+
+				//else create a new combobox with default values and append it to the combobox array
+				lineChartConfig.axisYConfig.push(newCombo);
+			}
+		},
+		removeLineAxisY: function(index) {		//function used to remove combobox entries for the configuration of the line chart visualization
+			if (lineChartConfig.axisYConfig.length>1)
+				lineChartConfig.axisYConfig.splice(index, 1);
+		},
+		getBarChartStatus: function() { return barChartConfig.initialized; },
+		addBarAxisY: function() {			// function used to add combobox entries for the configuration of the line chart visualization
+			// check if there is any combobox with default value
+			var nonSelectedCombo = _.where(barChartConfig.axisYConfig, {selected: ""});
+
+			// if there is an empty combobox or the number of comboboxes is equals with number
+			// of the line axis, stop the excecution of the function
+			if ( nonSelectedCombo.length>0 ||
+				barChartConfig.axisYConfig.length>2 ||
+				barChartConfig.axisYConfig.length == barChartConfig.axisY.length ) {
+
+				return false;
+			} else {
+				var newCombo = {
+					selected: "",
+					options: barChartConfig.axisY
+				};
+
+				//else create a new combobox with default values and append it to the combobox array
+				barChartConfig.axisYConfig.push(newCombo);
+			}
+		},
+		removeBarAxisY: function(index) {		//function used to remove combobox entries for the configuration of the line chart visualization
+			if (barChartConfig.axisYConfig.length>1)
+				barChartConfig.axisYConfig.splice(index, 1);
 		}
-	}]);
+	}
+}]);
