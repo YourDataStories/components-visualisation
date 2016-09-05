fb7a34cfacd4a627a3e3472742c56d88c36225b2
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-09-05 15:13:04 +0300

    Improve Dashboard look
    - smaller panel height
    - font awesome icons instead of bootstrap in buttons
    - cyan visualization panel heading color

--- demo/js/components/dashboard-visualization.js
+++ demo/js/components/dashboard-visualization.js
@@ -1,17 +1,26 @@
-angular.module('yds').directive('ydsDashboardVisualization', ['CountrySelectionService',
-    function(CountrySelectionService) {
+angular.module('yds').directive('ydsDashboardVisualization', ['CountrySelectionService', '$ocLazyLoad',
+    function(CountrySelectionService, $ocLazyLoad) {
         return {
             restrict: 'E',
             scope: {
                 elementH: '@'   // Height of component
             },
-            templateUrl: ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath  +'/' :'') + 'templates/dashboard-visualization.html',
+            templateUrl: ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath +'/' : '') + 'templates/dashboard-visualization.html',
             link: function (scope, element, attrs) {
                 // Check if the component's height attribute is defined, else assign default value
                 if (_.isUndefined(scope.elementH) || scope.elementH.trim() == "")
                     scope.elementH = 300;
 
-                scope.selectedVis = "";
+                scope.selectedVis = "bar";
+
+                // Load FontAwesome CSS file
+                var drupalPath = ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath + '/' : '');
+                $ocLazyLoad.load({
+                    files: [
+                        drupalPath + "css/font-awesome.min.css"
+                    ],
+                    cache: true
+                });
 
                 /**
                  * Change selected visualization type and if there is a year range selected, add it to apiOptions
