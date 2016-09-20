bab762cd864807914e7380f61edce8913b88c907
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-09-20 12:00:10 +0300

    Merge dashboard-grid and dashboard-info into dashboard-updater

--- demo/js/components/dashboard-updater.js
+++ demo/js/components/dashboard-updater.js
@@ -1,25 +1,36 @@
-angular.module('yds').directive('ydsDashboardGrid', ['Data', '$timeout', 'DashboardService',
-    function(Data, $timeout, DashboardService) {
+angular.module('yds').directive('ydsDashboardUpdater', ['$timeout', 'DashboardService',
+    function($timeout, DashboardService) {
         return {
             restrict: 'E',
             scope: {
+                type: '@',          // What type of component to show (grid, info etc.)
+                viewType: '@',      // Type to give to component
                 dashboardId: '@',   // ID used for getting selected year range from DashboardService
                 lang: '@'           // Language of component
             },
-            templateUrl: ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath  +'/' :'') + 'templates/dashboard-grid.html',
+            templateUrl: ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath  +'/' :'') + 'templates/dashboard-updater.html',
             link: function (scope) {
                 var dashboardId = scope.dashboardId;
-                scope.showGrid = false;
+                scope.showInfo = false;
 
+                // If type is undefined, set default value
+                if (_.isUndefined(scope.type) || scope.type.trim() == "")
+                    scope.type = "info";
+
+                // If viewType is undefined, set default value
+                if (_.isUndefined(scope.viewType) || scope.viewType.trim() == "")
+                    scope.type = "default";
+
+                // If lang is undefined, set default value
                 if (_.isUndefined(scope.lang) || scope.lang.trim() == "")
                     scope.lang = "en";
 
-                // If dashboardId is undefined, show error
+                // If dashboardId is undefined, set default value
                 if (_.isUndefined(dashboardId) || dashboardId.trim() == "") {
                     dashboardId = "default";
                 }
 
-                var updateInfo = function() {
+                var updateExtraParams = function() {
                     var apiOptionsMap = DashboardService.getApiOptionsMapping();
 
                     // Get min and max selected year and create the year range string for request
@@ -46,20 +57,21 @@ angular.module('yds').directive('ydsDashboardGrid', ['Data', '$timeout', 'Dashbo
                     // Add new extraParams to scope
                     scope.extraParams = extraParams;
 
-                    // Re-render info component
-                    scope.showGrid = false;
+                    // Re-render component
+                    var type = scope.type;  // Save current type
+                    scope.type = "";        // Make type empty to hide component
 
                     $timeout(function() {
-                        scope.showGrid = true;
+                        scope.type = type;  // At end of digest show component again
                     });
                 };
 
                 // Subscribe to be notified of changes in selected countries and year range
-                DashboardService.subscribeSelectionChanges(scope, updateInfo);
-                DashboardService.subscribeYearChanges(scope, updateInfo);
+                DashboardService.subscribeSelectionChanges(scope, updateExtraParams);
+                DashboardService.subscribeYearChanges(scope, updateExtraParams);
 
                 // Get initial info
-                updateInfo();
+                updateExtraParams();
             }
         };
     }
