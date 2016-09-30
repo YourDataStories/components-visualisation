a5c91aea69300a8ffb1d0a06f4cbe7e14fd82c08
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-09-30 10:40:55 +0300

    Add country and year selection to Dashboard 2 for both sectors

--- demo/js/controllers/dashboard2.js
+++ demo/js/controllers/dashboard2.js
@@ -5,10 +5,26 @@ angular.module('yds').controller('Dashboard2Controller', ['$scope', 'DashboardSe
         // If the panels should be allowed to open
         scope.allowOpenPanel = false;
 
+        // Default selected sector
+        scope.selectedSector = "aidactivity";
+
         // Set initial panel titles
         scope.panelSectorTitle = "Choose your sector";
         scope.panelCountrySelectionTitle = "Choose countries";
         scope.panelTimePeriodTitle = "Choose time period of activities";
         scope.panelCategoryTitle = "Choose filter category";
+
+        /**
+         * Select a sector
+         * @param newSector Sector to select
+         */
+        scope.setSelectedSector = function(newSector) {
+            // Clear selected countries
+            DashboardService.clearCountries(scope.selectedSector);
+            DashboardService.clearCountries(newSector);
+
+            // Select new sector
+            scope.selectedSector = newSector;
+        }
     }
 ]);
