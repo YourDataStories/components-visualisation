e68d8c6e815bf2eee655cf20d00fc0fc16e4e9a7
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-10-18 15:40:22 +0300

    Add ability to show minfo to cache-info component

--- demo/js/components/cache-info.js
+++ demo/js/components/cache-info.js
@@ -4,6 +4,8 @@ angular.module('yds').directive('ydsCacheInfo', ['Data', '$timeout', function(Da
         scope: {},
         templateUrl: ((typeof Drupal != 'undefined')? Drupal.settings.basePath  + Drupal.settings.yds_project.modulePath  +'/' :'') + 'templates/cache-info.html',
         link: function (scope, element, attrs) {
+            scope.showMinfo = false;
+
             scope.truncateCache = function(tableName) {
                 Data.getCacheInfo(tableName)
                     .then(function(response) {
@@ -29,6 +31,16 @@ angular.module('yds').directive('ydsCacheInfo', ['Data', '$timeout', function(Da
                 Data.getCacheInfo()
                     .then(function(response) {
                         scope.tables = response.data;
+
+                        // If there is a view for minfo, show it in the page
+                        if (!_.isEmpty(response.view)) {
+                            var view = _.first(response.view);
+                            if (_.has(view, "minfo")) {
+                                scope.showMinfo = true;
+
+                                scope.mInfo = view.minfo;
+                            }
+                        }
                     }, function(error) {
                         console.error("Error when getting cache info: " + error);
                     });
