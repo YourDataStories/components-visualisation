09581a15d96a09c4b58b489a29fc172713525a96
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-11-21 11:36:30 +0200

    Add project info in Contracts Dashboard

--- demo/js/controllers/dashboard-p1.js
+++ demo/js/controllers/dashboard-p1.js
@@ -1,7 +1,8 @@
-angular.module('yds').controller('DashboardPilot1Controller', ['$scope', 'DashboardService',
-    function($scope, DashboardService) {
+angular.module('yds').controller('DashboardPilot1Controller', ['$scope', '$timeout', '$location', '$anchorScroll', '$window', 'DashboardService',
+    function($scope, $timeout, $location, $anchorScroll, $window, DashboardService) {
         var scope = $scope;
 
+        scope.showProjectInfo = false;
         scope.aggregateToShow = 0;
         scope.aggregateClasses = [];
 
@@ -25,5 +26,23 @@ angular.module('yds').controller('DashboardPilot1Controller', ['$scope', 'Dashbo
         // Set new aggregates
         scope.aggregates = aggregates.types;
         scope.aggregateTitles = aggregates.titles;
+
+        // Subscribe to be notified of selected project changes
+        DashboardService.subscribeProjectChanges(scope, function() {
+            if ($window.pageYOffset < 1000) {
+                // Scroll a bit to make details visible
+                $location.hash("dashboard-contract-data-grid");
+                $anchorScroll();
+            }
+
+            // Select new project
+            scope.showProjectInfo = false;
+            scope.selectedProject = {};
+
+            $timeout(function() {
+                scope.selectedProject = DashboardService.getSelectedProjectInfo();
+                scope.showProjectInfo = true;
+            });
+        });
     }
 ]);
