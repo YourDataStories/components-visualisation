022224d67ac447d6139c4a94a92e806464aa9a36
Leonidas Tsekouras <leotsekouras@gmail.com>
2016-12-30 17:59:45 +0200

    Use correct data format for Highcharts 4.2.6 in region selector

--- demo/js/components/region-selector-gr.js
+++ demo/js/components/region-selector-gr.js
@@ -4,15 +4,112 @@ angular.module('yds').directive('ydsRegionSelectorGr', ['Data', '$q',
             restrict: 'E',
             scope: {
                 elementH:'@'    // Height of the component in pixels
-                //todo: do not ignore element-h attribute
             },
             templateUrl: ((typeof Drupal != 'undefined')? Drupal.settings.basePath  + Drupal.settings.yds_project.modulePath  +'/' :'') + 'templates/region-selector-gr.html',
             link: function (scope, element) {
+                var elementH = parseInt(scope.elementH);
+
                 var regionSelContainer = angular.element(element[0].querySelector('.region-selector-container'));
 
                 //create a random id for the element that will render the plot
                 var elementId = "regionsel" + Data.createRandomId();
                 regionSelContainer[0].id = elementId;
+
+                //check if the component's height attr is defined, else assign default value
+                if(_.isUndefined(elementH) || _.isNaN(elementH))
+                    elementH = 300;
+
+                // Declare objects used in the configuration of the map later
+                var highDetail = null;
+                var lowDetail = null;
+                var states = {
+                    // States of map points
+                    hover: {
+                        color: '#e5ed04'
+                    },
+                    select: {
+                        color: '#a4edba',
+                        borderColor: 'black',
+                        dashStyle: 'shortdot'
+                    }
+                };
+
+                /**
+                 * Callback for when drillup happens. Clears the selected points from the chart,
+                 * and sets the chart's subtitle to empty
+                 */
+                var drillup = function () {
+                    var chart = this,
+                        points = [];
+                    setTimeout(function () {
+                        points = chart.getSelectedPoints();
+
+                        Highcharts.each(points, function (p) {
+                            // unselect points from previous drilldown
+                            p.selected = false;
+                            p.setState('', true);
+                        });
+                    }, 0);
+
+                    chart.setTitle(null, {text: ''});
+                };
+
+                /**
+                 * Callback for when drilldown happens. Gets the drilled-down point, and creates the series that should
+                 * be shown based on the drilldownMap
+                 * @param e
+                 */
+                var drilldown = function (e) {
+                    if (!e.seriesOptions  && e.point.selected) {
+                        var chart = this,
+                            mapKey = e.point.drilldown,
+                            regionName = e.point.name;
+                        chart.showLoading("Loading...");
+
+                        // Keep only the part of the map that we need
+                        var mapData = _.clone(highDetail);
+                        var drilldownData = [];
+
+                        if (_.has(drilldownMap, mapKey)) {
+                            mapData.features = _.filter(mapData.features, function(item) {
+                                return _.contains(drilldownMap[mapKey], item.properties.NAME_ENG);
+                            });
+                        }
+
+                        // For each prefecture to display, create a value of 0 for it so it is colored
+                        $.each(mapData.features, function () {
+                            drilldownData.push({
+                                name: this.properties.NAME_ENG,
+                                value: 0
+                            });
+                        });
+
+                        // Hide loading and add series
+                        chart.hideLoading();
+                        chart.addSeriesAsDrilldown(e.point, {
+                            name: regionName,
+                            data: drilldownData,
+                            mapData: mapData,
+                            joinBy: ['NAME_ENG', "name"],
+                            keys: ['NAME_ENG', 'value'],
+                            allowPointSelect: true,
+                            dataLabels: {
+                                enabled: true,
+                                format: '{point.properties.NAME_ENG}'
+                            },
+                            tooltip: {
+                                enabled: false,
+                                pointFormatter: function() {
+                                    return "<b>" + this.properties.NAME_ENG + "</b>";
+                                }
+                            },
+                            states: states
+                        });
+
+                    }
+
+                    this.setTitle(null, {text: regionName});
+                };
 
                 // Get map of regions -> prefectures
                 var drilldownMap = Data.getRegionToPrefectureMapGr();
@@ -24,8 +121,8 @@ angular.module('yds').directive('ydsRegionSelectorGr', ['Data', '$q',
                 ];
 
                 $q.all(promises).then(function(results) {
-                    var lowDetail = results[0];
-                    var highDetail = results[1];
+                    lowDetail = results[0];
+                    highDetail = results[1];
 
                     Highcharts.maps["countries/gr-low-res"] = lowDetail;
 
@@ -38,84 +135,24 @@ angular.module('yds').directive('ydsRegionSelectorGr', ['Data', '$q',
                         var code = item.properties.hasc;
                         item.drilldown = code;
                         if (_.has(drilldownMap, code)) {
-                            data.push([code, drilldownMap[code].length]);
+                            data.push({
+                                code: code,
+                                value: drilldownMap[code].length
+                            });
                         } else {
-                            data.push([code, 0]);
+                            data.push({
+                                code: code,
+                                value: 0
+                            });
                         }
                     });
 
                     new Highcharts.mapChart(elementId, {
                         chart: {
-                            height: 600,
-                            width: 500,
+                            height: elementH,
                             events: {
-                                drilldown: function (e) {
-                                    if (!e.seriesOptions  && e.point.selected) {
-                                        var chart = this,
-                                            mapKey = e.point.drilldown,
-                                            regionName = e.point.name;
-                                        chart.showLoading("Loading...");
-
-                                        // Keep only the part of the map that we need
-                                        var mapData = _.clone(highDetail);
-                                        var drilldownData = [];
-
-                                        if (_.has(drilldownMap, mapKey)) {
-                                            mapData.features = _.filter(mapData.features, function(item) {
-                                                return _.contains(drilldownMap[mapKey], item.properties.NAME_ENG);
-                                            });
-                                        }
-
-                                        // For each prefecture to display, create a value of 0 for it so it is colored
-                                        $.each(mapData.features, function () {
-                                            drilldownData.push([this.properties.NAME_ENG, 0]);
-                                        });
-
-                                        // Hide loading and add series
-                                        chart.hideLoading();
-                                        chart.addSeriesAsDrilldown(e.point, {
-                                            name: regionName,
-                                            data: drilldownData,
-                                            mapData: mapData,
-                                            joinBy: ['NAME_ENG', 0],
-                                            keys: ['NAME_ENG', 'value'],
-                                            allowPointSelect: true,
-                                            dataLabels: {
-                                                enabled: true,
-                                                format: '{point.properties.NAME_ENG}'
-                                            },
-                                            tooltip: {
-                                                enabled: false,
-                                                pointFormatter: function() {
-                                                    return "<b>" + this.properties.NAME_ENG + "</b>";
-                                                }
-                                            },
-                                            states: {
-                                                hover: {
-                                                    color: '#a4edba'
-                                                }
-                                            }
-                                        });
-
-                                    }
-
-                                    this.setTitle(null, {text: regionName});
-                                },
-                                drillup: function () {
-                                    var chart = this,
-                                        points = [];
-                                    setTimeout(function () {
-                                        points = chart.getSelectedPoints();
-
-                                        Highcharts.each(points, function (p) {
-                                            // unselect points from previous drilldown
-                                            p.selected = false;
-                                            p.setState('', true);
-                                        });
-                                    }, 0);
-
-                                    chart.setTitle(null, {text: ''});
-                                }
+                                drilldown: drilldown,
+                                drillup: drillup
                             }
                         },
                         title: {
@@ -145,20 +182,11 @@ angular.module('yds').directive('ydsRegionSelectorGr', ['Data', '$q',
                                 enabled: true,
                                 format: '{point.properties.name}'
                             },
-                            joinBy: ['hasc', 0],
+                            joinBy: ['hasc', 'code'],
                             keys: ['hasc', 'value'],
                             name: "Regions of Greece",
                             allowPointSelect: true,
-                            states: {
-                                hover: {
-                                    color: '#a4edba'
-                                },
-                                select: {
-                                    color: '#a4edba',
-                                    borderColor: 'black',
-                                    dashStyle: 'shortdot'
-                                }
-                            },
+                            states: states,
                             point: {
                                 events: {
                                     select: function() {
