a75f80d0fce6dc4b153061c7aac788e89078a1d3
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-09 13:51:29 +0200

    Add details page for "View" button of View Data tab in Public Works

--- demo/js/controllers/PublicWorksController.js
+++ demo/js/controllers/PublicWorksController.js
@@ -1,11 +1,30 @@
-angular.module('yds').controller('PublicWorksController', ['$scope',
-    function($scope) {
+angular.module('yds').controller('PublicWorksController', ['$scope', '$timeout', '$location', '$anchorScroll', '$window', 'DashboardService',
+    function($scope, $timeout, $location, $anchorScroll, $window, DashboardService) {
         var scope = $scope;
 
+        scope.showProjectInfo = false;
         scope.selector = "location";
 
         scope.setSelector = function(selector) {
             scope.selector = selector;
-        }
+        };
+
+        // Subscribe to be notified of selected project changes
+        DashboardService.subscribeProjectChanges(scope, function() {
+            if ($window.pageYOffset < 1000) {
+                // Scroll a bit to make details visible
+                $location.hash("dashboard-public-works-data-grid");
+                $anchorScroll();
+            }
+
+            // Select new project
+            scope.showProjectInfo = false;
+            scope.selectedProject = {};
+
+            $timeout(function() {
+                scope.selectedProject = DashboardService.getSelectedProjectInfo();
+                scope.showProjectInfo = true;
+            });
+        });
     }
 ]);
