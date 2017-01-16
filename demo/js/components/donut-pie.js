141b5258ff083c18e538b500a3d583afcbd852a8
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-16 13:04:15 +0200

    Selecting CPVs from the grids selects them on the donut pies

--- demo/js/components/donut-pie.js
+++ demo/js/components/donut-pie.js
@@ -26,8 +26,9 @@ angular.module('yds').directive('ydsDonutPie', ['Data', 'DashboardService', func
             var exporting = scope.exporting;
             var elementH = scope.elementH;
             var extraParams = scope.extraParams;
-            var dashboardId = scope.dashboardId;
             var selectionId = scope.selectionId;
+
+            var chart = null;
 
             //check if the projectId and the viewType attr is defined, else stop the process
             if (_.isUndefined(projectId) || projectId.trim()=="") {
@@ -102,7 +103,7 @@ angular.module('yds').directive('ydsDonutPie', ['Data', 'DashboardService', func
                         }
                     });
 
-                    new Highcharts.Chart(elementId, options);
+                    chart = new Highcharts.Chart(elementId, options);
 
                     // Remove loading animation
                     scope.loading = false;
@@ -115,6 +116,30 @@ angular.module('yds').directive('ydsDonutPie', ['Data', 'DashboardService', func
                     // Remove loading animation
                     scope.loading = false;
                 });
+
+            DashboardService.subscribeGridSelectionChanges(scope, function() {
+                // If the selection for this pie changed, select the appropriate CPV if it exists in one of the series
+                var selection = DashboardService.getGridSelection(selectionId);
+
+                if (!_.isEmpty(selection)) {
+                    // Get actual selection (it can be only one in this case)
+                    var idToSelect = _.first(selection).id;
+
+                    // Look in each series for the CPV with the ID to select
+                    _.each(chart.series, function(series) {
+                        _.each(series.points, function(point) {
+                            if (point.id == idToSelect) {
+                                point.select(true, false);
+                            }
+                        });
+                    });
+                } else {
+                    // Deselect all points
+                    _.each(chart.series, function(series) {
+                        _.first(series.points).select(false, false);
+                    })
+                }
+            });
         }
     };
 }]);
