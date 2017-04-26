82b5868bdb38c9de65218d5dfbcc7684b2de49c5
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-04-26 21:17:20 +0300

    Add Twitter Search component for some countries

---
+++ demo/js/components/twitter-search.js
@@ -0,0 +1,26 @@
+angular.module("yds").directive("ydsTwitterSearch", ["Data", "$timeout",
+    function (Data, $timeout) {
+        return {
+            restrict: "E",
+            scope: {
+                projectId: "@"     // ID of the project
+            },
+            templateUrl: ((typeof Drupal != "undefined") ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath + "/" : "") + "templates/twitter-search.html",
+            link: function (scope) {
+                scope.showWidget = false;
+
+                // Get countries of this project
+                Data.getProjectVis("heatmap", scope.projectId, "default", "en").then(function (response) {
+                    var countries = _.pluck(response.data, "code");
+
+                    // If we should, show the widget
+                    if (!_.isEmpty(countries) && countries.indexOf("ZW") !== -1 && countries.indexOf("NL") !== -1) {
+                        scope.showWidget = true;
+
+                        $timeout(twttr.widgets.load);
+                    }
+                });
+            }
+        };
+    }
+]);
