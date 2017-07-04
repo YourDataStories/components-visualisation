19e215eec0b9ba272ff39fa32c3a40db44d4f0b8
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-07-04 16:35:06 +0300

    Implement switching of dashboard types in Dynamic Dashboard page

--- demo/js/controllers/dashboard-dynamic.js
+++ demo/js/controllers/dashboard-dynamic.js
@@ -5,7 +5,7 @@ angular.module('yds').controller('DashboardDynamicController', ['$scope', '$time
         scope.dashboardsConfig = {
             types: DashboardService.getDashboardTypes(),
             selectedDashboard: "contract",
-            filters: DashboardService.getDashboardFilters("contract"),
+            filters: [],
             selectedFilters: []
         };
 
@@ -23,6 +23,45 @@ angular.module('yds').controller('DashboardDynamicController', ['$scope', '$time
 
             // Save the selected filters to the DashboardService
             DashboardService.saveObject("filter", scope.dashboardsConfig.selectedFilters);
+        };
+
+        /**
+         * Change the Dashboard type.
+         * @param newType
+         */
+        scope.changeDashboardType = function (newType) {
+            // Get new filters
+            scope.dashboardsConfig.filters = DashboardService.getDashboardFilters(newType);
+
+            // Update selected filters
+            scope.updateSelectedFilters();
+
+            // Update the aggregates
+            scope.aggregates = [];
+            scope.aggregateTitles = [];
+            scope.aggregateClasses = [];
+
+            $timeout(function () {
+                var aggregates = DashboardService.getAggregates(newType);
+
+                scope.aggregateClass = "col-md-" + aggregates.width;
+
+                // Set classes for tabs
+                scope.aggregateClasses = [];
+                _.each(aggregates.types, function (aggregate) {
+                    scope.aggregateClasses.push(aggregate.replace(/\./g, "-"));
+                });
+
+                // Set new aggregates
+                scope.aggregates = aggregates.types;
+                scope.aggregateTitles = aggregates.titles;
+                scope.selectTab(0);
+
+                // Need to select the active tab after they have been added with ng-repeat
+                $timeout(function () {
+                    scope.dashboardVisActiveTab = 0;
+                });
+            });
         };
 
         /**
@@ -47,33 +86,14 @@ angular.module('yds').controller('DashboardDynamicController', ['$scope', '$time
             scope.aggregateToShow = tabIndex;
         };
 
-        // Update selected filters, to select the ones that should be selected initially
-        scope.updateSelectedFilters();
-
-        // Get aggregates for sector
-        var aggregates = DashboardService.getAggregates("contract");
-        scope.aggregateClass = "col-md-" + aggregates.width;
-
-        // Reset uib-tabset
-        scope.dashboardVisActiveTab = 0;
-
-        // Set classes for tabs
-        scope.aggregateClasses = [];
-        _.each(aggregates.types, function (aggregate) {
-            scope.aggregateClasses.push(aggregate.replace(/\./g, "-"));
-        });
-
-        // Set new aggregates
-        scope.aggregates = aggregates.types;
-        scope.aggregateTitles = aggregates.titles;
-
-        scope.infoPopoverUrl = ((typeof Drupal != 'undefined') ? Drupal.settings.basePath + Drupal.settings.yds_project.modulePath + '/' : '') + "templates-demo/contracts-info.html";
+        // Select the contract Dashboard type as a default
+        scope.changeDashboardType("contract");
 
         // Subscribe to be notified of selected project changes
         DashboardService.subscribeProjectChanges(scope, function () {
             if ($window.pageYOffset < 1000) {
                 // Scroll a bit to make details visible
-                $location.hash("dashboard-contract-data-grid");
+                $location.hash("dashboard-data-grid");
                 $anchorScroll();
             }
 
