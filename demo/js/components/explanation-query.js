94057595fbe60d83226301eea91dd7634fdf4f7b
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-09-13 14:56:13 +0300

    Add grid-results support to explanation query, when grid API is used

--- demo/js/components/explanation-query.js
+++ demo/js/components/explanation-query.js
@@ -14,19 +14,45 @@ angular.module("yds").directive("ydsExplanationQuery", ["$location", "Search", "
                 var viewType = params.viewType;
                 var lang = params.lang;
 
+                var useGridApi = params.gridapi;
+                var gridType = params.gridtype;
+
                 // All other parameters are put in the extra parameters object
-                var extraParams = _.omit(params, "chart", "id", "viewType", "lang");
+                var extraParams = _.omit(params, "chart", "id", "viewType", "lang", "gridapi", "gridtype");
 
                 // Add explain_calls parameter so the server will explain the query
                 extraParams.explain_calls = 1;
 
                 // Simulate the component's original request
-                Data.getProjectVis(chartType, projectId, viewType, lang, extraParams)
-                    .then(function (response) {
-                        scope.queries = response.data;
-                    }, function (error) {
-                        console.error("Error while getting explanation for component!", error);
-                    });
+                if (chartType !== "grid" || gridType === "grid") {
+                    // Use regular project visualization function
+                    Data.getProjectVis(chartType, projectId, viewType, lang, extraParams)
+                        .then(function (response) {
+                            scope.queries = response.data;
+                        }, function (error) {
+                            console.error("Error while getting explanation for component!", error);
+                        });
+                } else if (gridType === "grid-results") {
+                    // Remove regular parameters from extraParams
+                    extraParams = _.omit(extraParams, "projectId", "pagingGrid");
+
+                    // Only send language once if it was added twice
+                    if (_.isArray(lang)) {
+                        lang = _.first(lang);
+                    }
+
+                    // For grid-results we need to check which API to call
+                    if (useGridApi === "true") {
+                        Data.getProjectVis("grid", projectId, viewType, lang, extraParams)
+                            .then(function (response) {
+                                scope.queries = response.data;
+                            }, function (error) {
+                                console.error("Error while getting explanation for component!", error);
+                            });
+                    } else {
+                        //todo: search API should be used
+                    }
+                }
             }
         };
     }
