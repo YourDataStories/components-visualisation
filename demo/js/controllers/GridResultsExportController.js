9786836fdf9de653a7da616d088a8eb148d7b91c
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-12 13:26:00 +0300

    Add select/deselect all buttons to grid results export modal

--- demo/js/controllers/GridResultsExportController.js
+++ demo/js/controllers/GridResultsExportController.js
@@ -16,5 +16,15 @@ angular.module("yds").controller("GridResultsExportModalCtrl", ["$scope", "modal
 
             $scope.$close(selection);
         };
+
+        /**
+         * Make all items in the modal selected or deselected, based on the given boolean value
+         * @param selected  True to select all, false to deselect all
+         */
+        $scope.selectAll = function (selected) {
+            _.each($scope.modalConfig.view, function (item) {
+                item.selected = selected;
+            });
+        };
     }
 ]);
