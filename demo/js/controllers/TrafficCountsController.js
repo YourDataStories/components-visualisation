f532ecaa0602ede8b74aa1c06890ad37057556fa
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-16 15:27:18 +0300

    Add grid with contracts related to the clicked point

--- demo/js/controllers/TrafficCountsController.js
+++ demo/js/controllers/TrafficCountsController.js
@@ -3,6 +3,7 @@ angular.module("yds").controller("TrafficCountsController", ["$scope",
         var scope = $scope;
 
         scope.projectId = "http://linkedeconomy.org/resource/Contract/AwardNotice/2013208591/5795646";
+        scope.lang = "en";
 
         // Create the object for the clicked traffic point
         scope.clickedTrafficPoint = {
