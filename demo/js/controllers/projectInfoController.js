1d948f3fb92371d3de36e27d705f352ae2c7c3bf
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-18 12:57:51 +0300

    Show full Contract details in Galway Traffic page

--- demo/js/controllers/projectInfoController.js
+++ demo/js/controllers/projectInfoController.js
@@ -1,8 +1,10 @@
-angular.module('yds').controller('ProjectInfoController', ['$scope', 'DashboardService',
-    function($scope, DashboardService) {
+angular.module("yds").controller("ProjectInfoController", ["$scope", "DashboardService", "YDS_CONSTANTS",
+    function ($scope, DashboardService, YDS_CONSTANTS) {
         // Get info for selected project from DashboardService
         var projectInfo = DashboardService.getSelectedProjectInfo();
-
         $scope.projectId = projectInfo.id;
+
+        // Set base URL variable
+        $scope.baseUrl = YDS_CONSTANTS.PROJECT_DETAILS_URL;
     }
 ]);
