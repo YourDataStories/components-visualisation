42816ff3bf6b6b3e862da61da66ca41722d8a39c
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-25 16:39:40 +0300

    Add year and time selection to the slider

--- demo/js/components/dashboard/time-slider-selector.js
+++ demo/js/components/dashboard/time-slider-selector.js
@@ -20,7 +20,6 @@ angular.module("yds").directive("ydsTimeSlider", ["$timeout", "DashboardService"
                 var maxYear = parseInt(scope.maxYear);
                 var height = parseInt(scope.height);
                 var dashboardId = scope.dashboardId;
-                var vertical = scope.vertical;
 
                 // Check if type attribute is set correctly
                 if (_.isUndefined(type) || (type !== "year" && type !== "day" && type !== "time")) {
@@ -46,29 +45,58 @@ angular.module("yds").directive("ydsTimeSlider", ["$timeout", "DashboardService"
                     }
                 }
 
+                // Set names for the days of the week
+                var weekDays = [
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday",
+                    "Sunday"
+                ];
+
+                // Set common options for all sliders
                 var options = {};
+                var defaultValue = null;
+
+                // Set options that depend on the slider's type
+                var specificOptions;
                 switch (type) {
                     case "year":
+                        specificOptions = {
+                            floor: minYear,
+                            ceil: maxYear
+                        };
+
+                        defaultValue = minYear;
                         break;
                     case "day":
-                        options.stepsArray = [
-                            "Monday",
-                            "Tuesday",
-                            "Wednesday",
-                            "Thursday",
-                            "Friday",
-                            "Saturday",
-                            "Sunday"
-                        ];
+                        specificOptions = {
+                            stepsArray: weekDays
+                        };
+
+                        defaultValue = 0;
                         break;
                     case "time":
+                        specificOptions = {
+                            floor: 0,
+                            step: 5,
+                            ceil: 1439, // There are 1440 minutes in a day
+                            translate: function (value) {
+                                // Transform the minutes into a time format
+                                var hours = Math.floor(value / 60);
+                                var minutes = value % 60;
+                                return (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+                            }
+                        };
                         break;
                 }
 
                 // Set slider options
                 scope.yearSlider = {
-                    value: minYear,
-                    options: options
+                    value: defaultValue,
+                    options: _.extend(options, specificOptions)
                 };
 
                 // Show angular slider after options are set
