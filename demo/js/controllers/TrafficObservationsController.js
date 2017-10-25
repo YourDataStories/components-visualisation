c7f1632eaaae93d311d17b3ff05c876c8ceb8eea
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-10-25 15:45:54 +0300

    Add map to Traffic Observations dashboard

--- demo/js/controllers/TrafficObservationsController.js
+++ demo/js/controllers/TrafficObservationsController.js
@@ -1,6 +1,17 @@
 angular.module("yds").controller("TrafficObservationsController", ["$scope", "$timeout",
     function ($scope, $timeout) {
         var scope = $scope;
+        scope.galwayProjectId = "http://linkedeconomy.org/resource/Contract/AwardNotice/2013208591/5795646";
+        scope.lang = "en";
 
+        // Create the object for the clicked traffic point
+        scope.selectedPoint = {
+            "point": null
+        };
+
+        // Watch changes in the selected point, and set it as selected in the DashboardService
+        scope.$watch("selectedPoint.point", function () {
+            //todo: react to changes
+        });
     }
 ]);
