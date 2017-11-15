8e1af2373f5e9b637fafc9d3a5d850ee11373eb2
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-11-15 12:49:38 +0200

    Improve formatting of embed and basket buttons code

--- demo/js/components/chart-buttons/basket-button.js
+++ demo/js/components/chart-buttons/basket-button.js
@@ -1,116 +1,118 @@
-angular.module('yds').directive('ydsBasketBtn', ['$compile', 'Data', 'Basket', 'Filters',
-	function($compile, Data, Basket, Filters) {
-	return {
-		restrict: 'A',
-		link: function (scope, element, attrs) {
-			var projectId = scope.projectId;
-			var viewType = scope.viewType;
-			var lang = scope.lang;
+angular.module("yds").directive("ydsBasketBtn", ["$compile", "Data", "Basket", "Filters",
+    function ($compile, Data, Basket, Filters) {
+        return {
+            restrict: "A",
+            link: function (scope, element, attrs) {
+                var projectId = scope.projectId;
+                var viewType = scope.viewType;
+                var lang = scope.lang;
 
-			var elementClass = attrs.class;
-			var visualisationType = "";
-			var defaultVisTypes = ["pie", "line", "scatter", "bubble", "bar", "tree", "map", "grid", "result"];
-			
-			//if projectId or viewType attr is undefined, stop the execution of the directive
-			if (angular.isUndefined(projectId)|| angular.isUndefined(viewType)) {
-				scope.addToBasket = false;
-				return false;
-			}
+                var elementClass = attrs.class;
+                var visualisationType = "";
+                var defaultVisTypes = ["pie", "line", "scatter", "bubble", "bar", "tree", "map", "grid", "result"];
 
-			//check if the language attr is defined, else assign default value
-			if(angular.isUndefined(lang))
-				lang = "en";
+                // If projectId or viewType attr is undefined, stop the execution of the directive
+                if (_.isUndefined(projectId) || _.isUndefined(viewType)) {
+                    scope.addToBasket = false;
+                    return false;
+                }
 
-			//check if the element has id, accepted: "pie-container, line-container, bar-container, map-container"
-			if (!angular.isUndefined(elementClass) && elementClass!="") {
-				var visTypeFound = false;
+                // Check if the language attr is defined, else assign default value
+                if (_.isUndefined(lang))
+                    lang = "en";
 
-				for (var i=0; i<defaultVisTypes.length; i++) {				//check if the main element is a valid yds container
-					var visTypeIndex = elementClass.indexOf(defaultVisTypes[i]);
+                // Check if the element has id, accepted: "pie-container, line-container, bar-container, map-container"
+                if (!_.isUndefined(elementClass) && elementClass !== "") {
+                    var visTypeFound = false;
 
-					//if the one of the defaultVisTypes exist in the element id, get the visualisation type
-					if (visTypeIndex>-1) {
-						visualisationType = defaultVisTypes[i];
-						visTypeFound = true;
-						break;
-					}
-				}
+                    for (var i = 0; i < defaultVisTypes.length; i++) {				// Check if the main element is a valid yds container
+                        var visTypeIndex = elementClass.indexOf(defaultVisTypes[i]);
 
-				if (!visTypeFound) {
-					console.error('The basket extension cannot be applied on the selected element');
-					return false;
-				}
-			} else
-				return false;
+                        // If the one of the defaultVisTypes exist in the element id, get the visualisation type
+                        if (visTypeIndex > -1) {
+                            visualisationType = defaultVisTypes[i];
+                            visTypeFound = true;
+                            break;
+                        }
+                    }
 
-			//check if the user has enabled the embedding of the selected element
-			var enableBasket = scope.addToBasket;
+                    if (!visTypeFound) {
+                        console.error("The basket extension cannot be applied on the selected element");
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
 
-			if (!_.isUndefined(enableBasket) && enableBasket=="true"){
-				if (!angular.isUndefined(projectId) /*&& !isNaN(projectId)*/){
-					var basketBtnX = scope.basketBtnX;		//indicates the x position the embed button
-					var basketBtnY = scope.basketBtnY;		//indicates the y position the embed button
+                // Check if the user has enabled the embedding of the selected element
+                var enableBasket = scope.addToBasket;
 
-					//if embedBtnX property is undefined assign the default value
-					if (angular.isUndefined(basketBtnX) || isNaN(basketBtnX))
-						basketBtnX = 12;
+                if (!_.isUndefined(enableBasket) && enableBasket === "true") {
+                    if (!_.isUndefined(projectId)) {
+                        var basketBtnX = scope.basketBtnX;		// Indicates the x position the embed button
+                        var basketBtnY = scope.basketBtnY;		// Indicates the y position the embed button
 
-					//if embedBtnY property is undefined assign the default value
-					if (angular.isUndefined(basketBtnY) || isNaN(basketBtnY))
-						basketBtnY = 12;
+                        // If embedBtnX property is undefined assign the default value
+                        if (_.isUndefined(basketBtnX) || _.isNaN(basketBtnX))
+                            basketBtnX = 12;
 
-					var basketBtnTemplate = '<button type="button"' +
-						'class="btn btn-default btn-xs embed-btn"' +
-						'ng-click = "openBasketModal()">' +
-						'<i class="fa fa-shopping-basket" aria-hidden="true"></i>' +
-						'</button>';
+                        // If embedBtnY property is undefined assign the default value
+                        if (_.isUndefined(basketBtnY) || _.isNaN(basketBtnY))
+                            basketBtnY = 12;
 
-					//compile the embed button and append it to the element's container
-					var compiledTemplate = $compile(basketBtnTemplate)(scope);
-					compiledTemplate.css('position', 'absolute');
-					compiledTemplate.css('top', basketBtnY + 'px');
-					compiledTemplate.css('left', basketBtnX + 'px');
-				
-					compiledTemplate.css('font-size', 11 + 'px');
-					compiledTemplate.css('cursor', 'pointer');
+                        var basketBtnTemplate = "<button type='button' " +
+                            "class='btn btn-default btn-xs embed-btn' " +
+                            "ng-click = 'openBasketModal()'>" +
+                            "<i class='fa fa-shopping-basket' aria-hidden='true'></i>" +
+                            "</button>";
 
-					element.parent().append(compiledTemplate);
-				} else {
-					console.error('The embed extension is not configured properly');
-					return false;
-				}
-			} else {
-				return false;
-			}
+                        // Compile the embed button and append it to the element's container
+                        var compiledTemplate = $compile(basketBtnTemplate)(scope);
+                        compiledTemplate.css("position", "absolute");
+                        compiledTemplate.css("top", basketBtnY + "px");
+                        compiledTemplate.css("left", basketBtnX + "px");
 
-			/**
-			 * Open basket modal
-			 */
-			scope.openBasketModal = function() {
-				var userId = Basket.getUserId();
+                        compiledTemplate.css("font-size", 11 + "px");
+                        compiledTemplate.css("cursor", "pointer");
 
-				if (_.isUndefined(userId) || userId.length == 0) {
-					console.error("User ID is empty! Using default \"ydsUser\" as user ID...");
-					userId = "ydsUser";
-				}
+                        element.parent().append(compiledTemplate);
+                    } else {
+                        console.error("The embed extension is not configured properly");
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
 
-				var basketConfig = {
-					user_id: userId,
-					lang: lang,
-					type: "Dataset",
-					component_type: visualisationType,
-					content_type: viewType,
-					component_parent_uuid: projectId,
-					filters: Filters.get(element[0].id)
-				};
+                /**
+                 * Open basket modal
+                 */
+                scope.openBasketModal = function () {
+                    var userId = Basket.getUserId();
 
-				var modalRestrictions = {
-					Dataset: true,
-					Visualisation: true
-				};
-				
-				Basket.openModal(basketConfig, modalRestrictions);
-			};
-		}
-	}
-}]);
+                    if (_.isUndefined(userId) || userId.length === 0) {
+                        console.error("User ID is empty! Using default \"ydsUser\" as user ID...");
+                        userId = "ydsUser";
+                    }
+
+                    var basketConfig = {
+                        user_id: userId,
+                        lang: lang,
+                        type: "Dataset",
+                        component_type: visualisationType,
+                        content_type: viewType,
+                        component_parent_uuid: projectId,
+                        filters: Filters.get(element[0].id)
+                    };
+
+                    var modalRestrictions = {
+                        Dataset: true,
+                        Visualisation: true
+                    };
+
+                    Basket.openModal(basketConfig, modalRestrictions);
+                };
+            }
+        }
+    }
+]);
--- demo/js/components/chart-buttons/embed-button.js
+++ demo/js/components/chart-buttons/embed-button.js
@@ -1,124 +1,126 @@
-angular.module('yds').directive('ydsEmbed', ['$compile', 'Data', 'Filters', function($compile, Data, Filters) {
-	return {
-		restrict: 'A',
-		link: function (scope, element, attrs) {
-			var projectId = scope.projectId;
-			var viewType = scope.viewType;
+angular.module("yds").directive("ydsEmbed", ["$compile", "Data", "Filters", function ($compile, Data, Filters) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            var projectId = scope.projectId;
+            var viewType = scope.viewType;
 
-			var elementClass = attrs.class;
-			var embedCode = "";
-			var visualisationType = "";
-			var defaultVisTypes = ["pie", "line", "scatter", "bubble", "bar", "tree", "map", "grid"];
+            var elementClass = attrs.class;
+            var embedCode = "";
+            var visualisationType = "";
+            var defaultVisTypes = ["pie", "line", "scatter", "bubble", "bar", "tree", "map", "grid"];
 
-			//check if the element has one of the accepted IDs: "pie-container", "line-container", "bar-container", "map-container" etc.
-			if (!_.isUndefined(elementClass) && elementClass != "") {
-				var visTypeFound = false;
+            // Check if the element has one of the accepted IDs: "pie-container", "line-container", "bar-container", "map-container" etc.
+            if (!_.isUndefined(elementClass) && elementClass !== "") {
+                var visTypeFound = false;
 
-				for (var i=0; i<defaultVisTypes.length; i++) {				//check if the main element is a valid yds container
-					var visTypeIndex = elementClass.indexOf(defaultVisTypes[i]);
+                for (var i = 0; i < defaultVisTypes.length; i++) {				//check if the main element is a valid yds container
+                    var visTypeIndex = elementClass.indexOf(defaultVisTypes[i]);
 
-					//if the one of the defaultVisTypes exist in the element id, get the visualisation type
-					if (visTypeIndex>-1) {
-						visualisationType = defaultVisTypes[i];
-						visTypeFound = true;
-						break;
-					}
-				}
+                    // If the one of the defaultVisTypes exist in the element id, get the visualisation type
+                    if (visTypeIndex > -1) {
+                        visualisationType = defaultVisTypes[i];
+                        visTypeFound = true;
+                        break;
+                    }
+                }
 
-				if (!visTypeFound) {
-					console.error('The embed extension cannot be applied on the selected element');
-					return false;
-				}
-			} else {
-				return false;
-			}
+                if (!visTypeFound) {
+                    console.error("The embed extension cannot be applied on the selected element");
+                    return false;
+                }
+            } else {
+                return false;
+            }
 
-			//if projectId or viewType attr is undefined, stop the execution of the directive
-			if (_.isUndefined(projectId) || (_.isUndefined(viewType) && visualisationType != "map")) {
-				scope.embeddable = false;
-				return false;
-			}
+            // If projectId or viewType attr is undefined, stop the execution of the directive
+            if (_.isUndefined(projectId) || (_.isUndefined(viewType) && visualisationType !== "map")) {
+                scope.embeddable = false;
+                return false;
+            }
 
-			//check if the user has enabled the embedding of the selected element
-			var embeddable = scope.embeddable;
+            // Check if the user has enabled the embedding of the selected element
+            var embeddable = scope.embeddable;
 
-			if (!_.isUndefined(embeddable) && embeddable=="true"){
-				if (!_.isUndefined(projectId) /*&& !isNaN(projectId)*/){
-					var popoverPos = scope.popoverPos;		//indicates the position the popover
-					var embedBtnX = scope.embedBtnX;		//indicates the x position the embed button
-					var embedBtnY = scope.embedBtnY;		//indicates the y position the embed button
-					var defaultPos = ["top", "bottom", "left", "right"];
+            if (!_.isUndefined(embeddable) && embeddable === "true") {
+                if (!_.isUndefined(projectId)) {
+                    var popoverPos = scope.popoverPos;		// Indicates the position the popover
+                    var embedBtnX = scope.embedBtnX;		// Indicates the x position the embed button
+                    var embedBtnY = scope.embedBtnY;		// Indicates the y position the embed button
+                    var defaultPos = ["top", "bottom", "left", "right"];
 
-					//if popover position is undefined assign the default value
-					if (_.isUndefined(popoverPos) || _.indexOf(defaultPos, popoverPos)==-1)
-						popoverPos = "right";
+                    // If popover position is undefined assign the default value
+                    if (_.isUndefined(popoverPos) || _.indexOf(defaultPos, popoverPos) === -1)
+                        popoverPos = "right";
 
-					//if embedBtnX property is undefined assign the default value
-					if (_.isUndefined(embedBtnX) || _.isNaN(embedBtnX))
-						embedBtnX = 12;
+                    // If embedBtnX property is undefined assign the default value
+                    if (_.isUndefined(embedBtnX) || _.isNaN(embedBtnX))
+                        embedBtnX = 12;
 
-					//if embedBtnY property is undefined assign the default value
-					if (_.isUndefined(embedBtnY) || _.isNaN(embedBtnY))
-						embedBtnY = 12;
+                    // If embedBtnY property is undefined assign the default value
+                    if (_.isUndefined(embedBtnY) || _.isNaN(embedBtnY))
+                        embedBtnY = 12;
 
-					scope.popoverOpen = false;    //flag that indicates if the embed tooltip is shown
-					scope.popover = {
-						content: '',
-						templateUrl: Data.templatePath + 'templates/embed-popover.html',
-						title: 'Embed Code'
-					};
+                    scope.popoverOpen = false;    // Flag that indicates if the embed tooltip is shown
+                    scope.popover = {
+                        content: "",
+                        templateUrl: Data.templatePath + "templates/embed-popover.html",
+                        title: "Embed Code"
+                    };
 
-					var embedBtnTemplate = '<button type="button"' +
-							'class="btn btn-default btn-xs embed-btn"' +
-							'ng-click = "requestEmbed(this)"' +
-							'popover-trigger = "none"' +
-							'popover-is-open="popoverOpen"' +
-							'popover-placement="'+ popoverPos + '"' +
-							'uib-popover-template="popover.templateUrl">' +
-							'<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>' +
-							'</button>';
+                    var embedBtnTemplate = "<button type='button' " +
+                        "class='btn btn-default btn-xs embed-btn' " +
+                        "ng-click = 'requestEmbed(this)' " +
+                        "popover-trigger = 'none' " +
+                        "popover-is-open='popoverOpen' " +
+                        "popover-placement='" + popoverPos + "' " +
+                        "uib-popover-template='popover.templateUrl'>" +
+                        "<span class='glyphicon glyphicon-log-in' aria-hidden='true'></span>" +
+                        "</button>";
 
-					//compile the embed button and append it to the element's container
-					var compiledTemplate = $compile(embedBtnTemplate)(scope);
-					compiledTemplate.css('position', 'absolute');
-					compiledTemplate.css('top', embedBtnY + 'px');
-					compiledTemplate.css('left', embedBtnX + 'px');
-					compiledTemplate.css('z-index', 999);
-					compiledTemplate.css('font-size', 11 + 'px');
-					compiledTemplate.css('cursor', 'pointer');
+                    //compile the embed button and append it to the element's container
+                    var compiledTemplate = $compile(embedBtnTemplate)(scope);
+                    compiledTemplate.css("position", "absolute");
+                    compiledTemplate.css("top", embedBtnY + "px");
+                    compiledTemplate.css("left", embedBtnX + "px");
+                    compiledTemplate.css("z-index", 999);
+                    compiledTemplate.css("font-size", 11 + "px");
+                    compiledTemplate.css("cursor", "pointer");
 
-					element.parent().append(compiledTemplate);
-				} else {
-					console.error('The embed extension is not configured properly');
-					return false;
-				}
-			} else
-				return false;
+                    element.parent().append(compiledTemplate);
+                } else {
+                    console.error("The embed extension is not configured properly");
+                    return false;
+                }
+            } else {
+                return false;
+            }
 
-			scope.requestEmbed = function () {
-				if (embedCode=="") {          //if the request code doesn't exist
-					var facets = [{
-						facet_values: Filters.get(element[0].id).map(function(f) {
-							return JSON.stringify(f.attrs);
-						})
-					}];
-					var lang = (scope.lang || "en");	// if scope.lang is undefined, use English
+            scope.requestEmbed = function () {
+                if (embedCode === "") {          // If the request code doesn't exist
+                    var facets = [{
+                        facet_values: Filters.get(element[0].id).map(function (f) {
+                            return JSON.stringify(f.attrs);
+                        })
+                    }];
+                    var lang = (scope.lang || "en");	// If scope.lang is undefined, use English
 
-					Data.requestEmbedCode(projectId, facets, visualisationType, viewType, lang)
-					.then(function (response) {
-						embedCode = response.generated_hash;
-						scope.popover.content = '<iframe src="' +
-								'http://ydsdev.iit.demokritos.gr/YDSComponents/#!/embed/'+ embedCode +
-								'" style="width:600px; height:300px">' +
-								'<p>Your browser does not support iframes.</p>' +
-								'</iframe>';
-						this.popoverOpen = true;
-					}, function (error) {
-						console.log('request embed code error:', error);
-					});
-				} else
-					this.popoverOpen = !this.popoverOpen;
-			};
-		}
-	}
+                    Data.requestEmbedCode(projectId, facets, visualisationType, viewType, lang)
+                        .then(function (response) {
+                            embedCode = response.generated_hash;
+                            scope.popover.content = "<iframe src='" +
+                                "http://ydsdev.iit.demokritos.gr/YDSComponents/#!/embed/" + embedCode +
+                                "' style='width:600px; height:300px'>" +
+                                "<p>Your browser does not support iframes.</p>" +
+                                "</iframe>";
+                            scope.popoverOpen = true;
+                        }, function (error) {
+                            console.log("request embed code error:", error);
+                        });
+                } else {
+                    scope.popoverOpen = !scope.popoverOpen;
+                }
+            };
+        }
+    }
 }]);
