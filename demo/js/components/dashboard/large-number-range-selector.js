9f3bde78f08d053d46e2f14573be359c1bb5e337
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-12-01 16:17:38 +0200

    Add Selectize combobox to large number range selector

--- demo/js/components/dashboard/large-number-range-selector.js
+++ demo/js/components/dashboard/large-number-range-selector.js
@@ -11,23 +11,30 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
             },
             templateUrl: Data.templatePath + "templates/dashboard/large-number-range-selector.html",
             link: function (scope, element, attrs) {
-                scope.initialized = false;
+                var selectizeDiv = _.first(angular.element(element[0].querySelector(".large-number-range-select")));
+
+                // noinspection UnnecessaryLocalVariableJS
+                var elementId = "lnrs" + Data.createRandomId();
+                selectizeDiv.id = elementId;
 
                 var minValue = parseInt(scope.minValue);
                 var maxValue = parseInt(scope.maxValue);
                 var defaultValue = scope.defaultValue;
                 var selectionType = scope.selectionType;
 
-                // Check if minYear attribute is defined, else assign default value
+                // Check if minValue attribute is defined, else assign default value
                 if (_.isUndefined(minValue) || _.isNaN(minValue))
                     minValue = 0;
 
-                // Check if maxYear attribute is defined, else assign default value
+                // Check if maxValue attribute is defined, else assign default value
                 if (_.isUndefined(maxValue) || _.isNaN(maxValue))
                     maxValue = 100;
 
+                // todo: Generate this array from min/max values, e.g. https://stackoverflow.com/a/846249
+                var selectValues = [0, 1, 10, 100, 1000];
+
                 /**
-                 * Save the current value of the slider to the DashboardService using the specified selection type.
+                 * Save the current value of the slider to the DashboardService
                  */
                 var saveValueToDashboardService = function () {
                     DashboardService.saveObject(selectionType, scope.slider.value);
@@ -39,21 +46,23 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
                     defaultValue = cookieValue;
                 }
 
-                // Set slider options
-                scope.slider = {
-                    value: defaultValue,
-                    options: {
-                        onEnd: saveValueToDashboardService,
-                        floor: minValue,
-                        ceil: maxValue
-                    }
-                };
-
                 // Save initial slider value to DashboardService
-                saveValueToDashboardService();
+                // saveValueToDashboardService();
 
-                // Show angular slider after options are set
-                scope.initialized = true;
+                // Initialize Selectize.js
+                $(selectizeDiv).selectize({
+                    options: _.map(selectValues, function (item) {
+                        return {
+                            value: item,
+                            text: item
+                        };
+                    }),
+                    create: true,
+                    createOnBlur: true,
+                    maxItems: 1,
+                    placeholder: "Select " + scope.title
+                });
+                //todo: When adding items, check if they are numbers and between min/max values (see createFilter)
             }
         };
     }
