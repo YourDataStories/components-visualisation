5753075f663960bd9b80db765014245d65de48a1
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-12-01 16:36:18 +0200

    Keep object with currently selected min/max values

--- demo/js/components/dashboard/large-number-range-selector.js
+++ demo/js/components/dashboard/large-number-range-selector.js
@@ -16,7 +16,7 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
 
                 var minValue = parseInt(scope.minValue);
                 var maxValue = parseInt(scope.maxValue);
-                var defaultValue = scope.defaultValue;
+                // var defaultValue = scope.defaultValue;
                 var selectionType = scope.selectionType;
 
                 // Check if minValue attribute is defined, else assign default value
@@ -27,6 +27,11 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
                 if (_.isUndefined(maxValue) || _.isNaN(maxValue))
                     maxValue = 100;
 
+                var selection = {
+                    min: minValue,
+                    max: maxValue
+                };
+
                 // todo: Generate this array from min/max values, e.g. https://stackoverflow.com/a/846249
                 var selectValues = [0, 1, 10, 100, 1000];
 
@@ -34,19 +39,34 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
                  * Save the current value of the slider to the DashboardService
                  */
                 var saveValueToDashboardService = function () {
-                    DashboardService.saveObject(selectionType, scope.slider.value);
+                    DashboardService.saveObject(selectionType, selection);
                 };
 
-                // Check if there is a saved selection in the cookies, and use that as default
-                var cookieValue = DashboardService.getCookieObject(selectionType);
-                if (!_.isUndefined(cookieValue) && !_.isNull(cookieValue)) {
-                    defaultValue = cookieValue;
-                }
+                // todo: Check if there is a saved selection in the cookies, and use that as default
+                // var cookieValue = DashboardService.getCookieObject(selectionType);
+                // if (!_.isUndefined(cookieValue) && !_.isNull(cookieValue)) {
+                //     defaultValue = cookieValue;
+                // }
 
                 // Save initial slider value to DashboardService
-                // saveValueToDashboardService();
+                saveValueToDashboardService();
 
-                //todo: When adding items, check if they are numbers and between min/max values (see createFilter)
+                /**
+                 * Handle changes in the values of the Selectize.js comboboxes.
+                 * @param valName   Which value changed (min or max)
+                 * @param newValue  The new value
+                 */
+                var changeHandler = function (valName, newValue) {
+                    selection[valName] = newValue;
+
+                    $timeout(function () {
+                        // todo: Check that $timeout is required here
+                        saveValueToDashboardService();
+                    });
+                    console.log("Current selection: ", selection);
+                };
+
+                // todo: When adding items, check if they are numbers and between min/max values (see createFilter)
                 var selectizeOptions = {
                     options: _.map(selectValues, function (item) {
                         return {
@@ -57,12 +77,23 @@ angular.module("yds").directive("ydsLargeNumberRange", ["$timeout", "DashboardSe
                     create: true,
                     createOnBlur: true,
                     maxItems: 1,
-                    placeholder: "Select " + scope.title
+                    placeholder: "Select " + scope.title,
+                    onChange: function (item) {
+                        console.log("Selected ", item);
+                    }
                 };
 
                 // Initialize Selectize.js
-                $(minDiv).selectize(selectizeOptions);
-                $(maxDiv).selectize(selectizeOptions);
+                $(minDiv).selectize(_.extend(selectizeOptions, {
+                    onChange: function (item) {
+                        changeHandler("min", item);
+                    }
+                }));
+                $(maxDiv).selectize(_.extend(selectizeOptions, {
+                    onChange: function (item) {
+                        changeHandler("max", item);
+                    }
+                }));
             }
         };
     }
