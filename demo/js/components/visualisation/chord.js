fae8742326f92705b4c06d5ba40166afbb6229c9
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-04-18 17:49:00 +0300

    Load D3 library and display sample chord chart

--- demo/js/components/visualisation/chord.js
+++ demo/js/components/visualisation/chord.js
@@ -1,4 +1,4 @@
-angular.module("yds").directive("ydsChord", ["Data", "Filters", function (Data, Filters) {
+angular.module("yds").directive("ydsChord", ["$ocLazyLoad", "Data", "Filters", function ($ocLazyLoad, Data, Filters) {
     return {
         restrict: "E",
         scope: {
@@ -91,6 +91,129 @@ angular.module("yds").directive("ydsChord", ["Data", "Filters", function (Data, 
 
                             var options = response.data;
 
+                            //////////////////////// CHORD ////////////////////////
+                            var width = 300,
+                                height = 300,
+                                outerRadius = Math.min(width, height) / 2 - 10,
+                                innerRadius = outerRadius - 24;
+
+                            var formatPercent = d3.format(".1%");
+
+                            var arc = d3.svg.arc()
+                                .innerRadius(innerRadius)
+                                .outerRadius(outerRadius);
+
+                            var layout = d3.layout.chord()
+                                .padding(.04)
+                                .sortSubgroups(d3.descending)
+                                .sortChords(d3.ascending);
+
+                            var path = d3.svg.chord()
+                                .radius(innerRadius);
+
+                            var svg = d3.select(".chord-container").append("svg")
+                                .attr("width", width)
+                                .attr("height", height)
+                                .append("g")
+                                .attr("id", "circle")
+                                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+                            svg.append("circle")
+                                .attr("r", outerRadius);
+
+                            var items = [{
+                                name: "Test 1",
+                                color: "#FF00FF"
+                            }, {
+                                name: "Test 2",
+                                color: "#FFFF00"
+                            }];
+
+                            var matrix = [
+                                [0, 0.05],
+                                [0.2, 0]
+                            ];
+                            ready(false, items, matrix);
+
+                            function ready(error, items, matrix) {
+                                if (error) throw error;
+                                console.log("running");
+
+                                // Compute the chord layout.
+                                layout.matrix(matrix);
+
+                                // Add a group per neighborhood.
+                                var group = svg.selectAll(".group")
+                                    .data(layout.groups)
+                                    .enter().append("g")
+                                    .attr("class", "group")
+                                ;
+                                // .on("mouseover", mouseover);
+
+                                // Add a mouseover title.
+                                group.append("title").text(function (d, i) {
+                                    return items[i].name + ": " + formatPercent(d.value) + " of origins";
+                                });
+
+                                // Add the group arc.
+                                var groupPath = group.append("path")
+                                    .attr("id", function (d, i) {
+                                        return "group" + i;
+                                    })
+                                    .attr("d", arc)
+                                    .style("fill", function (d, i) {
+                                        return items[i].color;
+                                    });
+
+                                // Add a text label.
+                                var groupText = group.append("text")
+                                    .attr("x", 6)
+                                    .attr("dy", 15);
+
+                                groupText.append("textPath")
+                                    .attr("xlink:href", function (d, i) {
+                                        return "#group" + i;
+                                    })
+                                    .text(function (d, i) {
+                                        return items[i].name;
+                                    });
+
+                                // Remove the labels that don't fit. :(
+                                groupText.filter(function (d, i) {
+                                    return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength();
+                                })
+                                    .remove();
+
+                                // Add the chords.
+                                var chord = svg.selectAll(".chord")
+                                    .data(layout.chords)
+                                    .enter().append("path")
+                                    .attr("class", "chord")
+                                    .style("fill", function (d) {
+                                        return items[d.source.index].color;
+                                    })
+                                    .attr("d", path);
+                                console.log("ran");
+
+                                // Add an elaborate mouseover title for each chord.
+                                // chord.append("title").text(function (d) {
+                                //     return cities[d.source.index].name
+                                //         + " → " + cities[d.target.index].name
+                                //         + ": " + formatPercent(d.source.value)
+                                //         + "\n" + cities[d.target.index].name
+                                //         + " → " + cities[d.source.index].name
+                                //         + ": " + formatPercent(d.target.value);
+                                // });
+                                //
+                                // function mouseover(d, i) {
+                                //     chord.classed("fade", function (p) {
+                                //         return p.source.index != i
+                                //             && p.target.index != i;
+                                //     });
+                                // }
+                            }
+                            //////////////////////// CHORD ////////////////////////
+
                             console.log("Chord", options);
                             scope.chordData = options;
                         } else {
@@ -111,8 +234,17 @@ angular.module("yds").directive("ydsChord", ["Data", "Filters", function (Data, 
                     });
             };
 
-            // Create the bar
-            createChord();
+            if (!_.has(window, "d3")) {
+                $ocLazyLoad.load({
+                    files: ["https://d3js.org/d3.v3.min.js"],
+                    cache: true
+                }).then(function () {
+                    createChord();
+                });
+            } else {
+                // Create the bar
+                createChord();
+            }
         }
     };
 }]);
