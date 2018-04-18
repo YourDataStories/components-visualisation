1c4ba81cfc9b1f08a9f2920a05366dcc2449896d
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-04-18 17:50:31 +0300

    Remove function that is not needed

--- demo/js/components/visualisation/chord.js
+++ demo/js/components/visualisation/chord.js
@@ -133,85 +133,79 @@ angular.module("yds").directive("ydsChord", ["$ocLazyLoad", "Data", "Filters", f
                                 [0, 0.05],
                                 [0.2, 0]
                             ];
-                            ready(false, items, matrix);
 
-                            function ready(error, items, matrix) {
-                                if (error) throw error;
-                                console.log("running");
+                            // Compute the chord layout.
+                            layout.matrix(matrix);
 
-                                // Compute the chord layout.
-                                layout.matrix(matrix);
+                            // Add a group per neighborhood.
+                            var group = svg.selectAll(".group")
+                                .data(layout.groups)
+                                .enter().append("g")
+                                .attr("class", "group")
+                            ;
+                            // .on("mouseover", mouseover);
 
-                                // Add a group per neighborhood.
-                                var group = svg.selectAll(".group")
-                                    .data(layout.groups)
-                                    .enter().append("g")
-                                    .attr("class", "group")
-                                ;
-                                // .on("mouseover", mouseover);
+                            // Add a mouseover title.
+                            group.append("title").text(function (d, i) {
+                                return items[i].name + ": " + formatPercent(d.value) + " of origins";
+                            });
 
-                                // Add a mouseover title.
-                                group.append("title").text(function (d, i) {
-                                    return items[i].name + ": " + formatPercent(d.value) + " of origins";
+                            // Add the group arc.
+                            var groupPath = group.append("path")
+                                .attr("id", function (d, i) {
+                                    return "group" + i;
+                                })
+                                .attr("d", arc)
+                                .style("fill", function (d, i) {
+                                    return items[i].color;
                                 });
 
-                                // Add the group arc.
-                                var groupPath = group.append("path")
-                                    .attr("id", function (d, i) {
-                                        return "group" + i;
-                                    })
-                                    .attr("d", arc)
-                                    .style("fill", function (d, i) {
-                                        return items[i].color;
-                                    });
+                            // Add a text label.
+                            var groupText = group.append("text")
+                                .attr("x", 6)
+                                .attr("dy", 15);
 
-                                // Add a text label.
-                                var groupText = group.append("text")
-                                    .attr("x", 6)
-                                    .attr("dy", 15);
-
-                                groupText.append("textPath")
-                                    .attr("xlink:href", function (d, i) {
-                                        return "#group" + i;
-                                    })
-                                    .text(function (d, i) {
-                                        return items[i].name;
-                                    });
-
-                                // Remove the labels that don't fit. :(
-                                groupText.filter(function (d, i) {
-                                    return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength();
+                            groupText.append("textPath")
+                                .attr("xlink:href", function (d, i) {
+                                    return "#group" + i;
                                 })
-                                    .remove();
+                                .text(function (d, i) {
+                                    return items[i].name;
+                                });
 
-                                // Add the chords.
-                                var chord = svg.selectAll(".chord")
-                                    .data(layout.chords)
-                                    .enter().append("path")
-                                    .attr("class", "chord")
-                                    .style("fill", function (d) {
-                                        return items[d.source.index].color;
-                                    })
-                                    .attr("d", path);
-                                console.log("ran");
+                            // Remove the labels that don't fit. :(
+                            groupText.filter(function (d, i) {
+                                return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength();
+                            })
+                                .remove();
 
-                                // Add an elaborate mouseover title for each chord.
-                                // chord.append("title").text(function (d) {
-                                //     return cities[d.source.index].name
-                                //         + " → " + cities[d.target.index].name
-                                //         + ": " + formatPercent(d.source.value)
-                                //         + "\n" + cities[d.target.index].name
-                                //         + " → " + cities[d.source.index].name
-                                //         + ": " + formatPercent(d.target.value);
-                                // });
-                                //
-                                // function mouseover(d, i) {
-                                //     chord.classed("fade", function (p) {
-                                //         return p.source.index != i
-                                //             && p.target.index != i;
-                                //     });
-                                // }
-                            }
+                            // Add the chords.
+                            var chord = svg.selectAll(".chord")
+                                .data(layout.chords)
+                                .enter().append("path")
+                                .attr("class", "chord")
+                                .style("fill", function (d) {
+                                    return items[d.source.index].color;
+                                })
+                                .attr("d", path);
+
+                            // Add an elaborate mouseover title for each chord.
+                            // chord.append("title").text(function (d) {
+                            //     return cities[d.source.index].name
+                            //         + " → " + cities[d.target.index].name
+                            //         + ": " + formatPercent(d.source.value)
+                            //         + "\n" + cities[d.target.index].name
+                            //         + " → " + cities[d.source.index].name
+                            //         + ": " + formatPercent(d.target.value);
+                            // });
+                            //
+                            // function mouseover(d, i) {
+                            //     chord.classed("fade", function (p) {
+                            //         return p.source.index != i
+                            //             && p.target.index != i;
+                            //     });
+                            // }
+
                             //////////////////////// CHORD ////////////////////////
 
                             console.log("Chord", options);
